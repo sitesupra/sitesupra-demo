a8ea82d64f0f912e277379040cb5176a7bfdf5f1
Aigars Gedroics <aigars@videinfra.com>
2011-05-24 12:05:42 +0000

    Docrtine 2.0.5
    git-svn-id: http://svn.vig/supra7/trunk@49 f05b830f-2085-0410-a479-8f75d39d0b34

--- bin/doctrine.php
+++ bin/doctrine.php
@@ -18,39 +18,41 @@ if (file_exists($configFile)) {
     require $configFile;
     
     foreach ($GLOBALS as $helperSetCandidate) {
-        if ($helperSetCandidate instanceof \Symfony\Components\Console\Helper\HelperSet) {
+        if ($helperSetCandidate instanceof \Symfony\Component\Console\Helper\HelperSet) {
             $helperSet = $helperSetCandidate;
             break;
         }
     }
 }
 
-$helperSet = ($helperSet) ?: new \Symfony\Components\Console\Helper\HelperSet();
+$helperSet = ($helperSet) ?: new \Symfony\Component\Console\Helper\HelperSet();
 
-$cli = new \Symfony\Components\Console\Application('Doctrine Command Line Interface', Doctrine\Common\Version::VERSION);
-$cli->setCatchExceptions(false);
-$cli->setHelperSet($helperSet);
-$cli->addCommands(array(
-    // DBAL Commands
-    new \Doctrine\DBAL\Tools\Console\Command\RunSqlCommand(),
-    new \Doctrine\DBAL\Tools\Console\Command\ImportCommand(),
+\Doctrine\ORM\Tools\Console\ConsoleRunner::run($helperSet);
 
-    // ORM Commands
-    new \Doctrine\ORM\Tools\Console\Command\ClearCache\MetadataCommand(),
-    new \Doctrine\ORM\Tools\Console\Command\ClearCache\ResultCommand(),
-    new \Doctrine\ORM\Tools\Console\Command\ClearCache\QueryCommand(),
-    new \Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand(),
-    new \Doctrine\ORM\Tools\Console\Command\SchemaTool\UpdateCommand(),
-    new \Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand(),
-    new \Doctrine\ORM\Tools\Console\Command\EnsureProductionSettingsCommand(),
-    new \Doctrine\ORM\Tools\Console\Command\ConvertDoctrine1SchemaCommand(),
-    new \Doctrine\ORM\Tools\Console\Command\GenerateRepositoriesCommand(),
-    new \Doctrine\ORM\Tools\Console\Command\GenerateEntitiesCommand(),
-    new \Doctrine\ORM\Tools\Console\Command\GenerateProxiesCommand(),
-    new \Doctrine\ORM\Tools\Console\Command\ConvertMappingCommand(),
-    new \Doctrine\ORM\Tools\Console\Command\RunDqlCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand(),
-
-));
-
-$cli->run();
+//$cli = new \Symfony\Component\Console\Application('Doctrine Command Line Interface', Doctrine\Common\Version::VERSION);
+//$cli->setCatchExceptions(false);
+//$cli->setHelperSet($helperSet);
+//$cli->addCommands(array(
+//    // DBAL Commands
+//    new \Doctrine\DBAL\Tools\Console\Command\RunSqlCommand(),
+//    new \Doctrine\DBAL\Tools\Console\Command\ImportCommand(),
+//
+//    // ORM Commands
+//    new \Doctrine\ORM\Tools\Console\Command\ClearCache\MetadataCommand(),
+//    new \Doctrine\ORM\Tools\Console\Command\ClearCache\ResultCommand(),
+//    new \Doctrine\ORM\Tools\Console\Command\ClearCache\QueryCommand(),
+//    new \Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand(),
+//    new \Doctrine\ORM\Tools\Console\Command\SchemaTool\UpdateCommand(),
+//    new \Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand(),
+//    new \Doctrine\ORM\Tools\Console\Command\EnsureProductionSettingsCommand(),
+//    new \Doctrine\ORM\Tools\Console\Command\ConvertDoctrine1SchemaCommand(),
+//    new \Doctrine\ORM\Tools\Console\Command\GenerateRepositoriesCommand(),
+//    new \Doctrine\ORM\Tools\Console\Command\GenerateEntitiesCommand(),
+//    new \Doctrine\ORM\Tools\Console\Command\GenerateProxiesCommand(),
+//    new \Doctrine\ORM\Tools\Console\Command\ConvertMappingCommand(),
+//    new \Doctrine\ORM\Tools\Console\Command\RunDqlCommand(),
+//	new \Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand(),
+//
+//));
+//
+//$cli->run();
