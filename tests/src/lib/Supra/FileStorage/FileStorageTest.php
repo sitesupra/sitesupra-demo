3c1d0d8559e06e97e4664fbe8ba745461aa7b08d
Dmitry Polovka <dmitryp@videinfra.com>
2011-07-25 13:22:41 +0000

    Upload Validation Filter test and added files folder to ignore
    git-svn-id: http://svn.vig/supra7/trunk@117 f05b830f-2085-0410-a479-8f75d39d0b34

--- tests/src/lib/Supra/FileStorage/FileStorageTest.php
+++ tests/src/lib/Supra/FileStorage/FileStorageTest.php
@@ -4,6 +4,7 @@ namespace Supra\Tests\FileStorage;
 
 use Supra\Tests\TestCase;
 use Supra\FileStorage\FileStorage;
+use Supra\FileStorage\UploadFilter;
 
 require_once 'PHPUnit/Extensions/OutputTestCase.php';
 
@@ -71,5 +72,39 @@ class FileStorageTest extends \PHPUnit_Extensions_OutputTestCase
 		$roots = $repo->getRootNodes();
 		1+1;
 	}
+	
+	public function testUploadFilterFail()
+	{	
+		// clean up
+		$query = self::getConnection()->createQuery("delete from Supra\FileStorage\Entity\MetaData");
+		$query->execute();
+		$query = self::getConnection()->createQuery("delete from Supra\FileStorage\Entity\Abstraction\File");
+		$query->execute();
+		
+		// file
+		$uploadFile = __DIR__ . DIRECTORY_SEPARATOR . 'fail.yaml';
 
+		$file = new \Supra\FileStorage\Entity\File();
+		self::getConnection()->persist($file);
+		
+		$fileName = baseName($uploadFile);
+		$fileSize = fileSize($uploadFile);
+		$file->setName($fileName);
+		$file->setSize($fileSize);
+		$finfo = finfo_open(FILEINFO_MIME_TYPE);
+		$mimeType = finfo_file($finfo, $uploadFile);
+		finfo_close($finfo);
+		$file->setMimeType($mimeType);
+		
+		$filestorage = FileStorage::getInstance();
+		
+		$failed = false;
+		try {
+			$filestorage->storeFileData($file, $uploadFile);
+		} catch (UploadFilter\UploadFilterException $e) {
+			$failed = true;
+		}
+		
+		$this->assertTrue($failed);
+	}
 }
