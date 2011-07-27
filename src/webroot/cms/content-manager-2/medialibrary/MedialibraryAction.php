6456380349918a54869572d83e69fb9c8d534fbf
Dmitry Polovka <dmitryp@videinfra.com>
2011-07-27 11:26:43 +0000

    Task #2224 Implement actual file upload
    git-svn-id: http://svn.vig/supra7/trunk@127 f05b830f-2085-0410-a479-8f75d39d0b34

--- src/webroot/cms/content-manager-2/medialibrary/MedialibraryAction.php
+++ src/webroot/cms/content-manager-2/medialibrary/MedialibraryAction.php
@@ -87,52 +87,8 @@ class MediaLibraryAction extends CmsActionController
 			/* @var $node \Supra\FileStorage\Entity\File */
 			$node = $repo->findOneById($id);
 
-			$isImage = $node->isMimeTypeImage($node->getMimeType());
-
-			if ($isImage) {
-				$type = self::TYPE_IMAGE;
-			} else {
-				$type = self::TYPE_FILE;
-			}
-
-//			$type = $this->getType($node->getMimeType());
-			//List of properties which should be returned for each file or image
-			$properties = isset($_GET['properties']) ? $_GET['properties'] : 'id';
-			$properties = explode(',', $properties);
-
-			$filePath = $node->getPath(DIRECTORY_SEPARATOR, true);
-
-			$output = array();
-
-			if ($type == self::TYPE_FILE) {
-				$output[] = array(
-					'title' => 'Report',
-					'filename' => 'report.xml',
-					'description' => 'Annual financial report for our shareholders bla bla blaaa',
-					'file_web_path' => $filePath,
-					'id' => $node->getId(),
-					'type' => $type
-				);
-			}
-
-			if ($type == self::TYPE_IMAGE) {
-				$output[] = array(
-					'id' => $node->getId(),
-					'type' => $type,
-					'title' => $node->getTitle(),
-					'filename' => $node->getName(),
-					'description' => 'Hardcoded Description',
-					'size' => $node->getSize(),
-					'sizes' => Array(
-						'60x60' => Array('id' => '60x60', 'width' => 60, 'height' => 60, 'external_path' => $filePath),
-						'200x200' => Array('id' => '200x200', 'width' => 200, 'height' => 150, 'external_path' => $filePath),
-						'original' => Array('id' => 'original', 'width' => 600, 'height' => 450, 'external_path' => $filePath),
-					),
-					'60x60_url' => $filePath,
-					'200x200_url' => $filePath,
-					'original_url' => $filePath,
-				);
-			}
+			$nodeOutput = $this->imageAndFileOutput($node);
+			$output = array($nodeOutput);
 
 			$return = array(
 				'totalRecords' => count($output),
@@ -145,17 +101,116 @@ class MediaLibraryAction extends CmsActionController
 
 	public function createAction()
 	{
-		1 + 1;
+
 	}
 
 	public function saveAction()
 	{
-		1 + 1;
+		
 	}
 
 	public function deleteAction()
 	{
 		1 + 1;
+	}
+
+	public function uploadAction()
+	{
+		if (isset($_FILES['file']) && empty($_FILES['file']['error'])) {
+			
+			$file = $_FILES['file'];
+
+			// FIXME: should doctrine entity manager be as file stogare parameter?
+			$fileStorage = FileStorage::getInstance();
+
+			// FIXME: getting default DEM right now
+			$em = \Supra\Database\Doctrine::getInstance()->getEntityManager();
+
+			$fileEntity = new \Supra\FileStorage\Entity\File();
+			$em->persist($fileEntity);
+
+			$fileEntity->setName($file['name']);
+			$fileEntity->setSize($file['size']);
+			$fileEntity->setMimeType($file['type']);
+
+			if ( ! empty($_POST['folder'])) {
+				
+				$folderId = $_POST['folder'];
+				
+				// TODO: currently FileRepository is not assigned to the file abstraction
+				// FIXME: store the classname as constant somewhere?
+				/* @var $repo FileRepository */
+				$repo = $em->getRepository('Supra\FileStorage\Entity\Abstraction\File');
+				/* @var $node \Supra\FileStorage\Entity\File */
+				$folder = $repo->findOneById($folderId);
+				
+				//TODO: some check on not existant folder
+				
+				$folder->addChild($fileEntity);
+			}
+
+			$fileData = new \Supra\FileStorage\Entity\MetaData('en');
+			$fileData->setMaster($fileEntity);
+			$fileData->setTitle($file['name']);
+
+			$filestorage = FileStorage::getInstance();
+			$filestorage->storeFileData($fileEntity, $file['tmp_name']);
+
+			$em->flush();
+
+			$output = $this->imageAndFileOutput($fileEntity);
+
+			$this->getResponse()->setResponseData($output);
+		}
+	}
+
+	private function imageAndFileOutput(&$node)
+	{
+		$isImage = $node->isMimeTypeImage($node->getMimeType());
+		
+		$type = null;
+
+		if ($isImage) {
+			$type = self::TYPE_IMAGE;
+		} else {
+			$type = self::TYPE_FILE;
+		}
+
+		$filePath = $node->getPath(DIRECTORY_SEPARATOR, true);
+
+		$output = null;
+
+		if ($type == self::TYPE_FILE) {
+			$output = array(
+				'title' => $node->getTitle(),
+				'filename' => $node->getName(),
+				'description' => 'Hardcoded Description',
+				'file_web_path' => $filePath,
+				'id' => $node->getId(),
+				'type' => $type
+			);
+		}
+
+		if ($type == self::TYPE_IMAGE) {
+			$output = array(
+				'id' => $node->getId(),
+				'type' => $type,
+				'title' => $node->getTitle(),
+				'filename' => $node->getName(),
+				'description' => 'Hardcoded Description',
+				'size' => $node->getSize(),
+				'sizes' => Array(
+					'60x60' => Array('id' => '60x60', 'width' => 60, 'height' => 60, 'external_path' => $filePath),
+					'200x200' => Array('id' => '200x200', 'width' => 200, 'height' => 150, 'external_path' => $filePath),
+					'original' => Array('id' => 'original', 'width' => 600, 'height' => 450, 'external_path' => $filePath),
+				),
+				'60x60_url' => $filePath,
+				'200x200_url' => $filePath,
+				'original_url' => $filePath,
+			);
+		}
+
+		return $output;
 	}
 
 }
