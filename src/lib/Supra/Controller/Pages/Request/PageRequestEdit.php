4a89c73d1468670114d0111dd397b4eb403d21dc
Aigars Gedroics <aigars@videinfra.com>
2011-09-15 15:50:50 +0000

    Task #2892 – moved publish to PageRequestEdit
    git-svn-id: http://svn.vig/supra7/trunk@382 f05b830f-2085-0410-a479-8f75d39d0b34

--- src/lib/Supra/Controller/Pages/Request/PageRequestEdit.php
+++ src/lib/Supra/Controller/Pages/Request/PageRequestEdit.php
@@ -2,10 +2,234 @@
 
 namespace Supra\Controller\Pages\Request;
 
+use Supra\Database\Doctrine\Hydrator\ColumnHydrator;
+use Doctrine\ORM\Query;
+use Supra\Controller\Pages\Entity;
+use Doctrine\ORM\EntityManager;
+
 /**
  * Request object for edit mode requests
  */
 class PageRequestEdit extends PageRequest
 {
-	//TODO: should overwrite the constructor, etc
+	/**
+	 * {@inheritdoc}
+	 * @var boolean
+	 */
+	protected $allowFlushing = true;
+	
+	/**
+	 * @param EntityManager $publicEm
+	 */
+	public function publish(EntityManager $publicEm)
+	{
+		$draftEm = $this->getDoctrineEntityManager();
+		
+		if ($draftEm == $publicEm) {
+			$this->log->debug("Publish doesn't do anything because CMS and public database connections are identical");
+			
+			return;
+		}
+		
+		$pageData = $this->getRequestPageData();
+		
+		$pageId = $pageData->getMaster()->getId();
+		$localeId = $pageData->getLocale();
+		$pageDataId = $pageData->getId();
+
+		$draftPage = $pageData->getMaster();
+
+		$pageData = $publicEm->merge($pageData);
+
+		/* @var $publicPage Entity\Abstraction\Page */
+		$publicPage = $publicEm->find(PageRequest::PAGE_ABSTRACT_ENTITY, $pageId);
+		$pageData->setMaster($publicPage);
+
+		// 1. Get all blocks to be copied
+		$draftBlocks = $this->getBlocksInPage($draftEm, $pageData);
+
+		// 2. Get all blocks existing in public
+		$existentBlocks = $this->getBlocksInPage($publicEm, $pageData);
+
+		// 3. Remove blocks in 2, not in 1, remove all referencing block properties first
+		$draftBlockIdList = Entity\Abstraction\Entity::collectIds($draftBlocks);
+		$existentBlockIdList = Entity\Abstraction\Entity::collectIds($existentBlocks);
+		$removedBlockIdList = array_diff($existentBlockIdList, $draftBlockIdList);
+
+		if ( ! empty($removedBlockIdList)) {
+			$this->removeBlocks($publicEm, $removedBlockIdList);
+		}
+
+		// 4. Merge all placeholders, don't delete not used, let's keep them
+		foreach ($draftBlocks as $block) {
+			$placeholder = $block->getPlaceHolder();
+			$publicEm->merge($placeholder);
+		}
+
+		// 5. Merge all blocks in 1
+		foreach ($draftBlocks as $block) {
+			$publicEm->merge($block);
+		}
+
+		// 6. Get properties to be copied (of a. self and b. template)
+		$draftProperties = $this->getBlockPropertySet();
+
+		// 7. For properties 5b get block, placeholder IDs, check their existance in public, get not existant
+		$missingBlockIdList = array();
+
+		// Searching for missing parent template blocks IDs in the public schema
+		$blockIdList = $draftProperties->getBlockIdList();
+		$parentTemplateBlockIdList = array_diff($blockIdList, $draftBlockIdList);
+		
+		if ( ! empty($parentTemplateBlockIdList)) {
+			$blockEntity = PageRequest::BLOCK_ENTITY;
+
+			$qb = $publicEm->createQueryBuilder();
+			$qb->from($blockEntity, 'b')
+					->select('b.id')
+					->where($qb->expr()->in('b', $parentTemplateBlockIdList));
+
+			$query = $qb->getQuery();
+			$existentBlockIdList = $query->getResult(ColumnHydrator::HYDRATOR_ID);
+
+			$missingBlockIdList = array_diff($parentTemplateBlockIdList, $existentBlockIdList);
+		}
+
+		// 8. Merge missing place holders from 7 (reset $locked property)
+		$draftPlaceHolderIdList = $this->getPlaceHolderIdList($draftEm, $missingBlockIdList);
+		$publicPlaceHolderIdList = $this->loadEntitiesByIdList($publicEm, PageRequest::PLACE_HOLDER_ENTITY, $draftPlaceHolderIdList, 'e.id', ColumnHydrator::HYDRATOR_ID);
+		$missingPlaceHolderIdList = array_diff($draftPlaceHolderIdList, $publicPlaceHolderIdList);
+
+		$missingPlaceHolders = $this->loadEntitiesByIdList($draftEm, PageRequest::PLACE_HOLDER_ENTITY, $missingPlaceHolderIdList);
+
+		/* @var $placeHolder Entity\Abstraction\PlaceHolder */
+		foreach ($missingPlaceHolders as $placeHolder) {
+			$placeHolder = $publicEm->merge($placeHolder);
+
+			// Reset locked property
+			if ($placeHolder instanceof Entity\TemplatePlaceHolder) {
+				$placeHolder->setLocked(false);
+			}
+		}
+
+		// 9. Merge missing blocks (add $temporary property)
+		$missingBlocks = $this->loadEntitiesByIdList($draftEm, PageRequest::TEMPLATE_BLOCK_ENTITY, $missingBlockIdList);
+
+		/* @var $block Entity\TemplateBlock */
+		foreach ($missingBlocks as $block) {
+			$block = $publicEm->merge($block);
+			$block->setTemporary(true);
+		}
+
+		// 10. Merge all properties from 5
+		foreach ($draftProperties as $property) {
+			$publicEm->merge($property);
+		}
+
+		$publicEm->flush();
+	}
+	
+	/**
+	 * Loads blocks from the current page
+	 * @param EntityManager $em
+	 * @param Entity\Abstraction\Data $data
+	 * @return array 
+	 */
+	private function getBlocksInPage(EntityManager $em, Entity\Abstraction\Data $data)
+	{
+		$masterId = $data->getMaster()->getId();
+		$locale = $data->getLocale();
+		$blockEntity = PageRequest::BLOCK_ENTITY;
+		
+		$dql = "SELECT b FROM $blockEntity b 
+				JOIN b.placeHolder p
+				WHERE p.master = ?0 AND b.locale = ?1";
+		
+		$blocks = $em->createQuery($dql)
+				->setParameters(array($masterId, $locale))
+				->getResult();
+		
+		return $blocks;
+	}
+	
+	/**
+	 * Removes blocks with all properties by ID
+	 * @param EntityManager $em
+	 * @param array $blockIdList
+	 */
+	private function removeBlocks(EntityManager $em, array $blockIdList)
+	{
+		if (empty($blockIdList)) {
+			return;
+		}
+		
+		$blockPropetyEntity = PageRequest::BLOCK_PROPERTY_ENTITY;
+		$blockEntity = PageRequest::BLOCK_ENTITY;
+		
+		$qb = $em->createQueryBuilder();
+		$qb->delete($blockPropetyEntity, 'p')
+				->where($qb->expr()->in('p.block', $blockIdList))
+				->getQuery()
+				->execute();
+		
+		$qb = $em->createQueryBuilder();
+		$qb->delete($blockEntity, 'b')
+				->where($qb->expr()->in('b', $blockIdList))
+				->getQuery()
+				->execute();
+	}
+	
+	/**
+	 * Load place holder ID list from block ID list
+	 * @param EntityManager $em
+	 * @param array $blockIdList
+	 * @return array
+	 */
+	private function getPlaceHolderIdList(EntityManager $em, array $blockIdList)
+	{
+		if (empty($blockIdList)) {
+			return array();
+		}
+		
+		$qb = $em->createQueryBuilder();
+		$qb->from(PageRequest::BLOCK_ENTITY, 'b')
+				->join('b.placeHolder', 'p')
+				->select('DISTINCT p.id')
+				->where($qb->expr()->in('b', $blockIdList));
+		$query = $qb->getQuery();
+		$placeHolderIdList = $query->getResult(ColumnHydrator::HYDRATOR_ID);
+		
+		return $placeHolderIdList;
+	}
+	
+	/**
+	 * Loads entities by ID list
+	 * @param EntityManager $em
+	 * @param string $entity
+	 * @param array $idList 
+	 * @return array
+	 */
+	private function loadEntitiesByIdList(EntityManager $em, $entity, array $idList, $select = 'e', $hydrationMode = Query::HYDRATE_OBJECT)
+	{
+		if (empty($idList)) {
+			return array();
+		}
+		
+		$qb = $em->createQueryBuilder();
+		$qb->from($entity, 'e')
+				->select($select)
+				->where($qb->expr()->in('e', $idList));
+		$query = $qb->getQuery();
+		$list = $query->getResult($hydrationMode);
+		
+		return $list;
+	}
+	
+	/**
+	 * Disallows automatic flushing
+	 */
+	public function blockFlushing()
+	{
+		$this->allowFlushing = false;
+	}
 }
