b96a4c7194193dc85411d5db6e9948606c36ee00
Aigars Gedroics <aigars@videinfra.com>
2011-11-29 16:32:15 +0000

    The "caller parent already declared" check is disabled now because "When an object is destroyed, its hash may be reused for other objects".
    git-svn-id: http://svn.vig/supra7/trunk@676 f05b830f-2085-0410-a479-8f75d39d0b34

--- tests/src/lib/Supra/ObjectRepository/ObjectRepositoryTest.php
+++ tests/src/lib/Supra/ObjectRepository/ObjectRepositoryTest.php
@@ -213,14 +213,15 @@ class ObjectRepositoryTest extends \PHPUnit_Framework_TestCase
 		ObjectRepository::getObject('xxx', 'stdClass');
 	}
 
-	/**
-	 * @expectedException \Supra\ObjectRepository\Exception\RuntimeException
-	 */
-	public function testCallerHierarchyDuplicateCallerParent()
-	{
-		ObjectRepository::setCallerParent('xxx', 'yyy\\zzz');
-		ObjectRepository::setCallerParent('xxx', 'vvv\\aaa');
-	}
+	// See comment inside setCallerParent method
+//	/**
+//	 * @expectedException \Supra\ObjectRepository\Exception\RuntimeException
+//	 */
+//	public function testCallerHierarchyDuplicateCallerParent()
+//	{
+//		ObjectRepository::setCallerParent('xxx', 'yyy\\zzz');
+//		ObjectRepository::setCallerParent('xxx', 'vvv\\aaa');
+//	}
 
 	/**
 	 * Made for full code coverage
