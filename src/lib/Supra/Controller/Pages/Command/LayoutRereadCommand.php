9b89d3aea56cf5592584db661442b006c8ab5ade
Aigars Gedroics <aigars@videinfra.com>
2011-11-29 10:55:30 +0000

    Added layout reread CLI command, layout processor task now updates layouts as well
    git-svn-id: http://svn.vig/supra7/trunk@660 f05b830f-2085-0410-a479-8f75d39d0b34

---
+++ src/lib/Supra/Controller/Pages/Command/LayoutRereadCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Supra\Controller\Pages\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Supra\ObjectRepository\ObjectRepository;
+use Supra\Controller\Pages\Entity\Layout;
+use Supra\Controller\Layout\Processor\TwigProcessor;
+use Supra\Controller\Pages\Task\LayoutProcessorTask;
+use Supra\Controller\Layout\Exception\LayoutException;
+
+/**
+ * Rereads known layouts and updates their information
+ */
+class LayoutRereadCommand extends Command
+{
+	/**
+     * Configures the current command.
+     */
+    protected function configure()
+    {
+		$this->setName('su:layout:update')
+				->setDescription("Rereads the known layouts and updates their information in database");
+    }
+	
+	protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $entityManager = ObjectRepository::getEntityManager($this);
+		$layoutDao = $entityManager->getRepository(Layout::CN());
+		$layouts = $layoutDao->findAll();
+		
+		//TODO: fixed processor class us used now, should be configurable somehow
+		$layoutProcessor = new TwigProcessor();
+		$layoutProcessor->setLayoutDir(SUPRA_TEMPLATE_PATH);
+		
+		$layoutUpdateTask = new LayoutProcessorTask();
+		$layoutUpdateTask->setEntityManager($entityManager);
+		$layoutUpdateTask->setLayoutProcessor($layoutProcessor);
+		
+		foreach ($layouts as $layout) {
+			/* @var $layout Layout */
+			$file = $layout->getFile();
+			$layoutUpdateTask->setLayoutId($file);
+			
+			try {
+				$layoutUpdateTask->perform();
+			} catch (LayoutException $e) {
+				/* @var $e \Exception */
+				$output->writeln("<error>Exception was caught: {$e->getMessage()}</error>");
+			}
+//			$layoutUpdateTask->getLayout();
+		}
+		
+		$entityManager->flush();
+		
+		$message = $layoutUpdateTask->getStatisticsMessage();
+		
+		$output->writeln("<info>{$message}</info>");
+    }
+}
--- src/lib/Supra/Controller/Pages/Task/LayoutProcessorTask.php
+++ src/lib/Supra/Controller/Pages/Task/LayoutProcessorTask.php
@@ -6,13 +6,15 @@ use Supra\Controller\Layout\Processor\ProcessorInterface;
 use Supra\Controller\Pages\Entity;
 use Doctrine\ORM\EntityManager;
 use Supra\Cms\Exception\CmsException;
-use Supra\Controller\Layout\Exception as LayoutException;
 
 /**
  * Inserts or updates the layout using layout processor
  */
 class LayoutProcessorTask
 {
+	const STAT_LAYOUT = 'layouts';
+	const STAT_PLACEHOLDERS = 'placeholders';
+	
 	/**
 	 * Layout name, usually filename
 	 * @var string
@@ -34,7 +36,20 @@ class LayoutProcessorTask
 	 * @var Entity\Layout
 	 */
 	protected $layout;
+	
+	/**
+	 * @var array
+	 */
+	protected $statistics;
 
+	/**
+	 * Resets the statistics array
+	 */
+	public function __construct()
+	{
+		$this->resetStatistics();
+	}
+	
 	/**
 	 * @param string $layoutId
 	 */
@@ -66,12 +81,45 @@ class LayoutProcessorTask
 	{
 		return $this->layout;
 	}
+	
+	/**
+	 * @return array
+	 */
+	public function getStatistics()
+	{
+		return $this->statistics;
+	}
+	
+	/**
+	 * @return array
+	 */
+	public function resetStatistics()
+	{
+		$this->statistics = array(
+			self::STAT_LAYOUT => array(),
+			self::STAT_PLACEHOLDERS => array(),
+		);
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getStatisticsMessage()
+	{
+		$newLayoutCount = count($this->statistics[self::STAT_LAYOUT]);
+		$newPlaceHoldersCount = count($this->statistics[self::STAT_PLACEHOLDERS]);
+		
+		$message = "$newLayoutCount new layouts, $newPlaceHoldersCount new placeholders";
+		
+		return $message;
+	}
 
 	/**
 	 * Inserts or updates layout information in the storage
 	 */
 	public function perform()
 	{
+		$this->layout = null;
 		$file = $this->layoutId;
 
 		// Search for this layout
@@ -82,24 +130,28 @@ class LayoutProcessorTask
 			$layout = new Entity\Layout();
 			$this->entityManager->persist($layout);
 			$layout->setFile($file);
-			$processor = $this->layoutProcessor;
 			
-			try {
-				$places = $processor->getPlaces($file);
+			$this->statistics[self::STAT_LAYOUT][] = $file;
+		}
+		
+		$placeHolders = $layout->getPlaceHolders();
+		
+		$processor = $this->layoutProcessor;
+		$places = $processor->getPlaces($file);
+
+		foreach ($places as $name) {
+			$placeHolder = null;
+			
+			if ($placeHolders->containsKey($name)) {
+				$placeHolder = $placeHolders->get($name);
+			} else {
+				$placeHolder = new Entity\LayoutPlaceHolder($name);
+				$placeHolder->setLayout($layout);
 				
-				foreach ($places as $name) {
-					$placeHolder = new Entity\LayoutPlaceHolder($name);
-					$placeHolder->setLayout($layout);
-				}
-			} catch (LayoutException\LayoutNotFoundException $e) {
-				throw new CmsException('template.error.layout_not_found', null, $e);
-			} catch (LayoutException\RuntimeException $e) {
-				throw new CmsException('template.error.layout_error', null, $e);
+				$this->statistics[self::STAT_PLACEHOLDERS][$file][] = $name;
 			}
-			// Let it go..
-//			catch (\Exception $e) {
-//				throw new CmsException('template.error.create_internal_error', null, $e);
-//			}
+			
+			$this->entityManager->persist($placeHolder);
 		}
 
 		// Keep created layout
