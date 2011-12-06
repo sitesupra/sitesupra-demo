adf581e3ca28f0b799712fa0ccef31d5b72493f8
Eduard Fathelbajanov <eduard@videinfra.com>
2011-12-06 15:21:12 +0000

    commented out cron task | schema creation fails if schema is not created yet
    git-svn-id: http://svn.vig/supra7/trunk@711 f05b830f-2085-0410-a479-8f75d39d0b34

--- bin/supra.php
+++ bin/supra.php
@@ -28,8 +28,8 @@ $cli->addCommandClasses(array(
 //$cli->addCronJob('su:schema:update', 
 //		new \Supra\Console\Cron\Period\EveryHourPeriod('30'));
 
-$cli->addCronJob('su:pages:process_scheduled', 
-		new \Supra\Console\Cron\Period\EveryHourPeriod('30'));
+//$cli->addCronJob('su:pages:process_scheduled', 
+//		new \Supra\Console\Cron\Period\EveryHourPeriod('30'));
 
 $cli->setHelperSet($helperSet);
 $cli->setCatchExceptions(false);
