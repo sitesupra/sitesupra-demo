4b5c42323ea32f465732d886b629c348a93a23f7
Aigars Gedroics <aigars@videinfra.com>
2011-12-16 14:27:39 +0000

    Logging for htmlspecialchars warning 3
    git-svn-id: http://svn.vig/supra7/trunk@877 f05b830f-2085-0410-a479-8f75d39d0b34

--- src/lib/Twig/Extension/Core.php
+++ src/lib/Twig/Extension/Core.php
@@ -513,7 +513,6 @@ function twig_escape_filter(Twig_Environment $env, $string, $type = 'html', $cha
 			$prevError = error_get_last();
 			
             $result = htmlspecialchars($string, ENT_QUOTES | ENT_SUBSTITUTE, $charset);
-			error_log($string);
 			
 			if (error_get_last() != $prevError) {
 				\Log::error("Error ", error_get_last(), " for string ", $string . ' and charset ' . $charset);
