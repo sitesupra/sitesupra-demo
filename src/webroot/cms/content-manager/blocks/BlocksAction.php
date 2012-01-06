57137e54639bc90ff257dc4eefca907e3bbdcd00
Dmitry Polovka <dmitryp@videinfra.com>
2012-01-06 16:00:06 +0000

    Task #4446 Page manager - group blocks
    git-svn-id: http://svn.vig/supra7/trunk@1122 f05b830f-2085-0410-a479-8f75d39d0b34

--- src/webroot/cms/content-manager/blocks/BlocksAction.php
+++ src/webroot/cms/content-manager/blocks/BlocksAction.php
@@ -6,36 +6,81 @@ use Supra\Cms\ContentManager\PageManagerAction;
 use Supra\Controller\Pages\BlockControllerCollection;
 use Supra\Request;
 use Supra\Response;
-use Supra\Controller\Pages\Configuration\BlockControllerConfiguration;
+use Supra\ObjectRepository\ObjectRepository;
+use Supra\Configuration\Exception\ConfigurationMissing;
 
 class BlocksAction extends PageManagerAction
 {
+
 	/**
 	 * Collects block definition information
 	 */
 	public function blocksAction()
 	{
+		$logger = ObjectRepository::getLogger($this);
 		$blockCollection = BlockControllerCollection::getInstance();
-		$configurationList = $blockCollection->getConfigurationList();
-		
-		$response = array();
-		$groupTitles = array();
-		$blockTitles = array();
-		
-		$response['groups'][]  = array(
-			'id' => 'siteFeatures',
-			'title' => 'Site Features',
-			'default' => false,
+		$blockConfigurationList = $blockCollection->getBlocksConfigurationList();
+
+		$groupConfigurationList = $blockCollection->getGroupsConfigurationList();
+
+		$response = array(
+			'blocks' => array(),
+			'groups' => array(),
 		);
-		
-		/* @var $conf BlockControllerConfiguration */
-		foreach ($configurationList as $blockId => $conf) {
-			
+
+		$isDefaultGroupSet = false;
+		$defaultGroupKey = 0;
+		$groupIds = array();
+		foreach ($groupConfigurationList as $groupId => $group) {
+			/* @var $group \Supra\Controller\Pages\Configuration\BlockControllerGroupConfiguration */
+
+			$isDefaultGroup = $group->default;
+			if ($isDefaultGroupSet && $isDefaultGroup) {
+				$logger->warn("Default group is already set to \"{$response['groups'][$defaultGroupKey]['title']}\". Current group \"{$group->title}\" will be set as secondary");
+				$isDefaultGroup = false;
+			}
+
+			if ($isDefaultGroup && ! $isDefaultGroupSet) {
+				$newDefaultGroupKey = end(array_keys($response['groups']));
+
+				if ( ! is_null($newDefaultGroupKey)) {
+					$defaultGroupKey = $newDefaultGroupKey ++;
+				}
+
+				$isDefaultGroupSet = true;
+			}
+
+			$response['groups'][] = array(
+				'id' => $group->id,
+				'title' => $group->title,
+				'default' => $isDefaultGroup,
+			);
+
+			$groupIds[] = $group->id;
+		}
+
+		if (empty($response['groups'])) {
+			throw new ConfigurationMissing('At least one block group should be configured');
+		}
+
+		if ( ! $isDefaultGroupSet) {
+			$response['groups'][$defaultGroupKey]['default'] = true;
+		}
+
+		foreach ($blockConfigurationList as $blockId => $conf) {
+			/* @var $conf \Supra\Controller\Pages\Configuration\BlockControllerConfiguration */
+
+			$blockGroup = $conf->groupId;
+			if (empty($blockGroup) || ! in_array($blockGroup, $groupIds)) {
+				$logger->warn("Block \"{$conf->id}#{$conf->title}\" has empty group id or there is no group with such id. Block will be added into default group");
+				$blockGroup = $response['groups'][$defaultGroupKey]['id'];
+			}
+
 			$controller = $blockCollection->getBlockController($blockId);
 			$propertyDefinition = (array) $controller->getPropertyDefinition();
-			
+
 			$properties = array();
-			
+
 			foreach ($propertyDefinition as $key => $property) {
 				/* @var $property \Supra\Editable\EditableInterface */
 				$properties[] = array(
@@ -45,30 +90,22 @@ class BlocksAction extends PageManagerAction
 					'label' => $property->getLabel(),
 					'value' => $property->getDefaultValue(),
 					'group' => $property->getGroupLabel(),
-				) + $property->getAdditionalParameters();
+				) 
+				+ $property->getAdditionalParameters();
+				
 			}
-			
-			$groupId = 'siteFeatures';
-//			if ( ! is_null($conf->group)) {
-//				$group = $conf->group;
-//			}
-			
+
 			$response['blocks'][] = array(
 				'id' => $conf->id,
-				'group' => $groupId,
+				'group' => $blockGroup,
 				'title' => $conf->title,
 				'description' => $conf->description,
 				'icon' => $conf->iconWebPath,
 				'classname' => $conf->cmsClassname,
 				'properties' => $properties,
 			);
-			
-			$groupTitles[] = $groupId;
-			$blockTitles[] = $conf->title;
 		}
-		
-		array_multisort($groupTitles, $blockTitles, $response);
-		
+
 		$this->getResponse()->setResponseData($response);
 	}
 
