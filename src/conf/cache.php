9015d162c3dcce1ded131483b9d7c2ccc870b480
Aigars Gedroics <aigars@videinfra.com>
2012-01-26 10:22:34 +0000

    Memcached server check added, default authentication domain brought back to supra7.vig.
    git-svn-id: http://svn.vig/supra7/trunk@1423 f05b830f-2085-0410-a479-8f75d39d0b34

--- src/conf/cache.php
+++ src/conf/cache.php
@@ -1,14 +1,24 @@
 <?php
 
 use Supra\ObjectRepository\ObjectRepository;
+use Doctrine\Common\Cache\MemcacheCache;
+
+$memcachedHost = 'localhost';
+$memcachedPort = 11211;
 
 $systemInfo = ObjectRepository::getSystemInfo('');
 
-$cache = new \Doctrine\Common\Cache\MemcacheCache();
-$memcache = new \Memcache();
-$memcache->addserver('127.0.0.1');
+$memcache = new Memcache();
+$memcache->addserver($memcachedHost, $memcachedPort);
+$status = $memcache->getversion();
+
+if ($status === false) {
+	\Log::fatal("Memcached server $memcachedHost:$memcachedPort not accessible");
+	die(SUPRA_ERROR_MESSAGE);
+}
+
+$cache = new MemcacheCache();
 $cache->setMemcache($memcache);
-//$cache = new \Doctrine\Common\Cache\ArrayCache();
 $cache->setNamespace($systemInfo->getSystemId());
 
 ObjectRepository::setDefaultCacheAdapter($cache);
