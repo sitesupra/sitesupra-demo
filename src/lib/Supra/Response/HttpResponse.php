sitesupra/sitesupra-demo

commit c45fb586afe8d7bf0f3d7008febf82b2698929a6

Task #5002 – cache configuration, store/get. No group dopping yet. Segfaults sometime.
git-svn-id: http://svn.vig/supra7/trunk@1551 f05b830f-2085-0410-a479-8f75d39d0b34

src/lib/Supra/Response/HttpResponse.php
@@ -128,6 +128,16 @@ class HttpResponse implements ResponseInterface
 	{
 		
 	}
+	
+	/**
+	 * @return array
+	 */
+	public function __sleep()
+	{
+		$fields = array_keys(get_object_vars($this));
+		
+		return $fields;
+	}
 
 	/**
 	 * Normalizes header name

src/conf/cache.php
@@ -3,22 +3,28 @@
 use Supra\ObjectRepository\ObjectRepository;
 use Doctrine\Common\Cache\MemcacheCache;
 
-$memcachedHost = 'localhost';
-$memcachedPort = 11211;
+if ( ! class_exists('Memcache')) {
+	$cache = new Doctrine\Common\Cache\ArrayCache();
+} else {
 
-$systemInfo = ObjectRepository::getSystemInfo('');
+	$memcachedHost = 'localhost';
+	$memcachedPort = 11211;
 
-$memcache = new Memcache();
-$memcache->addserver($memcachedHost, $memcachedPort);
-$status = $memcache->getversion();
+	$systemInfo = ObjectRepository::getSystemInfo('');
 
-if ($status === false) {
-	\Log::fatal("Memcached server $memcachedHost:$memcachedPort not accessible");
-	die(SUPRA_ERROR_MESSAGE);
+	$memcache = new Memcache();
+	$memcache->addserver($memcachedHost, $memcachedPort);
+	$status = $memcache->getversion();
+
+	if ($status === false) {
+		\Log::fatal("Memcached server $memcachedHost:$memcachedPort not accessible");
+		die(SUPRA_ERROR_MESSAGE);
+	}
+
+	$cache = new MemcacheCache();
+	$cache->setMemcache($memcache);
 }
 
-$cache = new MemcacheCache();
-$cache->setMemcache($memcache);
 $cache->setNamespace($systemInfo->getSystemId());
 
 ObjectRepository::setDefaultCacheAdapter($cache);

src/lib/Supra/AuditLog/Writer/AuditLogWriter.php
@@ -75,7 +75,7 @@ class AuditLogWriter extends AuditLogWriterAbstraction
 	 */
 	public function write($level, $component, $action, $message, $user = null, $data = array()) 
 	{
-		$loggerName = $this->name;
+		$loggerName = null;
 		$event = new AuditLogEvent($level, $component, $action, $message, $loggerName, $user, $data);
 		$this->logWriter->write($event);
 	}

src/lib/Supra/Authentication/AuthenticationController.php
@@ -17,6 +17,7 @@ use Supra\Session\SessionNamespace;
  */
 abstract class AuthenticationController extends ControllerAbstraction implements Controller\PreFilterInterface
 {
+
 	/**
 	 * Login page path
 	 * @var string
@@ -28,6 +29,7 @@ abstract class AuthenticationController extends ControllerAbstraction implements
 	 * @var string
 	 */
 	protected $checkSessionPath = 'check-session';
+
 	/**
 	 * Base path
 	 * @var string
@@ -59,11 +61,11 @@ abstract class AuthenticationController extends ControllerAbstraction implements
 	public function getPublicUrlList()
 	{
 		$trimFunction = function ($value) {
-			return trim($value, '/');
-		};
+					return trim($value, '/');
+				};
 		$list = array_map($trimFunction, $this->publicUrlList);
-		
-		return $this->publicUrlList;
+
+		return $list;
 	}
 
 	/**
@@ -146,12 +148,13 @@ abstract class AuthenticationController extends ControllerAbstraction implements
 		$sessionManager = ObjectRepository::getSessionManager($this);
 		$session = $sessionManager->getDefaultSessionNamespace();
 		/* @var $session SessionNamespace */
-		
+
 		// Unset the session data
 		unset($session->login, $session->message);
-		
+
 		$request = $this->getRequest();
-		$isPublicUrl = $this->isPublicUrl($request->getRequestUri());
+		$uri = $this->getRequest()->getActionString();
+		$isPublicUrl = $this->isPublicUrl($uri);
 
 		// Allow accessign public URL
 		if ($isPublicUrl) {
@@ -167,10 +170,8 @@ abstract class AuthenticationController extends ControllerAbstraction implements
 
 		$post = $this->getRequest()->isPost();
 		$userProvider = ObjectRepository::getUserProvider($this);
-		
-		$loginPath = $this->getLoginPath();
-		$uri = $this->getRequest()->getActionString();
 
+		$loginPath = trim($this->getLoginPath(), '/');
 		// if post request then check for login and password fields presence
 		if ($post) {
 
@@ -184,19 +185,19 @@ abstract class AuthenticationController extends ControllerAbstraction implements
 			$password = new AuthenticationPassword($plainPassword);
 
 			if ( ! empty($login)) {
-				
+
 				// Authenticating user
 				$user = null;
-				
+
 				try {
-					
+
 					// TODO: Maybe should be moved to some password policy guide with password expire features?
 					if ($password->isEmpty()) {
 						throw new Exception\WrongPasswordException("Empty passwords are not allowed");
 					}
-					
+
 					$user = $userProvider->authenticate($login, $password);
-					
+
 					$userProvider->signIn($user);
 
 					if ($xmlHttpRequest) {
@@ -208,15 +209,13 @@ abstract class AuthenticationController extends ControllerAbstraction implements
 
 					$auditLog = ObjectRepository::getAuditLogger($this);
 					$auditLog->info(null, 'login', "User '{$user->getEmail()}' logged in", $user);
-					
+
 					throw new StopRequestException("Login success");
-					
 				} catch (Exception\AuthenticationFailure $exc) {
 					//TODO: pass the failure message somehow
-					
 					// Login not successfull
 					$message = 'Incorrect login name or password';
-					
+
 					//TODO: i18n
 					if ($exc instanceof Exception\ExistingSessionLimitation) {
 						$message = $exc->getMessage();
@@ -225,15 +224,10 @@ abstract class AuthenticationController extends ControllerAbstraction implements
 					if ($xmlHttpRequest) {
 						$this->response->setCode(401);
 						$this->response->header('X-Authentication-Pre-Filter-Message', $message);
-						
 					} else {
 
 						$session->login = $login;
 						$session->message = $message;
-
-						$request = $this->request;
-
-						/* @var $request Request\HttpRequest */
 
 						// if authentication failed, we redirect user to login page
 						$path = new Path($loginPath);
@@ -274,7 +268,7 @@ abstract class AuthenticationController extends ControllerAbstraction implements
 				throw new StopRequestException("User not authenticated");
 			}
 		} else {
-			
+
 			// Redirect from login form if the session is active
 			if ($uri == $loginPath) {
 				$uri = $this->getSuccessRedirectUrl();
@@ -311,7 +305,7 @@ abstract class AuthenticationController extends ControllerAbstraction implements
 
 			if ($externalUrl === false) {
 				$redirectTo = '/' . trim($redirectTo, '/');
-				
+
 				return $redirectTo;
 			}
 		}

src/lib/Supra/Cache/CacheGroupManager.php
@@ -1,0 +1,67 @@
+<?php
+
+namespace Supra\Cache;
+
+use Supra\ObjectRepository\ObjectRepository;
+use Doctrine\Common\Cache\Cache;
+
+/**
+ * 
+ */
+class CacheGroupManager
+{
+	/**
+	 * @var Cache
+	 */
+	private static $cache;
+	
+	/**
+	 * Local cache
+	 * @var array
+	 */
+	private static $localCache = array();
+	
+	/**
+	 * Constructor 
+	 */
+	public function __construct()
+	{
+		if ( ! isset(self::$cache)) {
+			self::$cache = ObjectRepository::getCacheAdapter($this);
+		}
+	}
+	
+	private function getCacheName($group)
+	{
+		return __CLASS__ . '_' . $group;
+	}
+	
+	public function getRevision($group)
+	{
+		if (isset(self::$localCache[$group])) {
+			return self::$localCache[$group];
+		}
+		
+		$cacheName = $this->getCacheName($group);
+		$value = self::$cache->fetch($cacheName);
+		
+		if ($value === false) {
+			$value = $this->resetRevision($group, $cacheName);
+		} else {
+			self::$localCache[$group] = $value;
+		}
+		
+		return $value;
+	}
+	
+	public function resetRevision($group)
+	{
+		$cacheName = $this->getCacheName($group);
+		$value = mt_rand();
+		self::$cache->save($cacheName, $value);
+		
+		self::$localCache[$group] = $value;
+
+		return $value;
+	}
+}

src/lib/Supra/Controller/Pages/BlockControllerCollection.php
@@ -129,5 +129,5 @@ class BlockControllerCollection
 
 		return $controller;
 	}
-
+	
 }

src/lib/Supra/Controller/Pages/CachedBlockController.php
@@ -1,0 +1,38 @@
+<?php
+
+namespace Supra\Controller\Pages;
+
+use Supra\Request\RequestInterface;
+use Supra\Response\ResponseInterface;
+
+/**
+ * CachedBlockController
+ */
+class CachedBlockController extends BlockController
+{
+	public function __construct(ResponseInterface $response)
+	{
+		parent::__construct();
+		$this->response = $response;
+	}
+	
+	public function execute()
+	{
+		
+	}
+	
+	public function createResponse(RequestInterface $request)
+	{
+		return $this->response;
+	}
+	
+//	public function setBlockPropertySet()
+//	{
+//		
+//	}
+//	
+//	public function prepareTwigHelper()
+//	{
+//		
+//	}
+}

src/lib/Supra/Controller/Pages/Configuration/BlockControllerCacheConfiguration.php
@@ -1,0 +1,98 @@
+<?php
+
+namespace Supra\Controller\Pages\Configuration;
+
+use Supra\Configuration\ConfigurationInterface;
+use Supra\Controller\Pages\Entity\Abstraction\Localization;
+use Supra\Controller\Pages\Entity\Abstraction\Block;
+
+/**
+ * Content cache configuration
+ */
+class BlockControllerCacheConfiguration implements ConfigurationInterface
+{
+	/**
+	 * Whether cache is enabled
+	 * @var boolean
+	 */
+	public $enabled = true;
+	
+	/**
+	 * Does the content differs by request page
+	 * @var boolean
+	 */
+	public $global = false;
+	
+	/**
+	 * Cache groups
+	 * @var string[]
+	 */
+	public $groups = array();
+	
+	/**
+	 * Lifetime of the cache. Empty means cache does not expires.
+	 * @var string
+	 */
+	public $lifetime;
+	
+	public function configure()
+	{
+		
+	}
+	
+	/**
+	 * Get cache key where to store the block, return null if not cacheable
+	 * @param Localization $page
+	 * @param Block $block
+	 * @return string 
+	 */
+	public function getCacheKey(Localization $page, Block $block)
+	{
+		if ( ! $this->enabled) {
+			return;
+		}
+
+		// No cache if lifetime is negative
+		$lifetime = $this->getLifetime();
+		if ($lifetime < 0) {
+			return;
+		}
+		
+		$cacheGroups = array();
+		$cacheGroupManager = new \Supra\Cache\CacheGroupManager();
+		
+		// Cache always differs for different block instances for now
+		$cacheGroups[] = $block->getId();;
+
+		if ( ! $this->global) {
+			$cacheGroups[] = $page->getId();
+		}
+
+		foreach ((array) $this->groups as $group) {
+			$cacheGroups[] = $cacheGroupManager->getRevision($group);
+		}
+		
+		$cacheKey = __CLASS__ . '_' . implode('_', $cacheGroups);
+		
+		return $cacheKey;
+	}
+	
+	/**
+	 * Get cache lifetime. Zero for "no lifetime", -1 to disable the cache
+	 * @return int
+	 */
+	public function getLifetime()
+	{
+		if (empty($this->lifetime)) {
+			$lifetime = 0;
+		} elseif (is_numeric($this->lifetime)) {
+			$lifetime = (int) $this->lifetime;
+		} elseif (is_string($this->lifetime)) {
+			$lifetime = strtotime($this->lifetime) - time();
+		}
+		
+		$lifetime = max($lifetime, -1);
+		
+		return $lifetime;
+	}
+}

commit cb15228be8047dad12ac46bb6b11d0e164a286f3

Fixed response serializing, broken in r1551.
git-svn-id: http://svn.vig/supra7/trunk@1552 f05b830f-2085-0410-a479-8f75d39d0b34

src/lib/Supra/Response/HttpResponse.php
@@ -134,7 +134,7 @@ class HttpResponse implements ResponseInterface
 	 */
 	public function __sleep()
 	{
-		$fields = array_keys(get_object_vars($this));
+		$fields = array_keys(get_class_vars(__CLASS__));
 		
 		return $fields;
 	}