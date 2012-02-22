4fd5bec8497d0a6c185230028cd661c4a961ec22
Aleksey Antipov <aleskey.antipov@videinfra.com>
2012-02-22 17:27:01 +0000

    functionality implementation for #5063; SupraPortal remote login/remote user provider data communication implementation
    git-svn-id: http://svn.vig/supra7/trunk@1717 f05b830f-2085-0410-a479-8f75d39d0b34

---
+++ src/webroot/components/CmsRemoteLogin/Controller.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Project\CmsRemoteLogin;
+
+use Supra\Controller\SimpleController;
+use Supra\ObjectRepository\ObjectRepository;
+use Supra\Remote\Command\CommandOutputWithData;
+use Supra\Remote\Client\RemoteCommandService;
+use Symfony\Component\Console\Input\ArrayInput;
+use Supra\Console\Output\ArrayOutputWithData;
+use Supra\User\Entity as UserEntity;
+use SupraPortal\SiteUser\Entity as SiteUserEntity;
+
+class Controller extends SimpleController//\Supra\Controller\ControllerAbstraction
+{
+	
+	/**
+	 * @var \Supra\User\UserProvider;
+	 */
+	protected $userProvider;
+	
+	protected $remoteUrl;
+
+	public function execute()
+	{
+
+		$this->userProvider = ObjectRepository::getUserProvider($this);
+				
+		$remoteCommandService = new RemoteCommandService();
+
+		$input = new ArrayInput(array(
+					'command' => 'su:utility:get_user_by_token',
+					'token' => $this->getRequest()->getParameter('token'),
+				));
+
+		$output = new ArrayOutputWithData();
+		$commandResult = $remoteCommandService->execute('portal', $input, $output);
+
+		$user = $output->getData();
+
+		if ($user instanceof UserEntity\User) {
+
+			$this->processRemoteUserData($user);
+			
+		} else {
+			
+			$this->badCredential();
+			
+		}
+	}
+
+	protected function processRemoteUserData(\Supra\User\Entity\User $user)
+	{
+		$signedInUser = $this->userProvider->getSignedInUser();
+			
+		//If user already signed in
+		if($signedInUser instanceof \Supra\User\Entity\User) {
+			
+			//Sign out old user and sign in new
+			if($signedInUser->getId() != $user->getId()) {
+				$this->userProvider->signOut();
+				$this->userProvider->signIn($user);
+			}
+		} else {
+			//sign in user
+			$this->userProvider->signIn($user);
+		}
+		
+		$this->getResponse()->redirect('/cms');
+		
+	}
+
+	protected function badCredential()
+	{
+		$this->getResponse()->redirect('/cms');
+	}
+	
+}
