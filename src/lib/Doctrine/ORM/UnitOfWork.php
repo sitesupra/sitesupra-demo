7c452e69c6e5414703edc7c6c93416faf3408fb2
Aigars Gedroics <aigars@videinfra.com>
2012-03-07 14:17:10 +0000

    Merged Doctrine ORM fix r786 into 2.2.1.
    git-svn-id: http://svn.vig/supra7/trunk@1804 f05b830f-2085-0410-a479-8f75d39d0b34

--- src/lib/Doctrine/ORM/UnitOfWork.php
+++ src/lib/Doctrine/ORM/UnitOfWork.php
@@ -78,6 +78,11 @@ class UnitOfWork implements PropertyChangedListener
     private $identityMap = array();
 
     /**
+     * Fix for spl_object_hash reusage in case of garbage collection process on objects
+     */
+    private $splObjectHashMemory = array();
+
+    /**
      * Map of all identifiers of managed entities.
      * Keys are object ids (spl_object_hash).
      *
@@ -2236,6 +2241,8 @@ class UnitOfWork implements PropertyChangedListener
         if ($this->evm->hasListeners(Events::onClear)) {
             $this->evm->dispatchEvent(Events::onClear, new Event\OnClearEventArgs($this->em, $entityName));
         }
+
+        $this->splObjectHashMemory = array();
     }
 
     /**
@@ -2790,6 +2797,8 @@ class UnitOfWork implements PropertyChangedListener
         $this->originalEntityData[$oid] = $data;
 
         $this->addToIdentityMap($entity);
+
+        $this->splObjectHashMemory[$oid] = $entity;
     }
 
     /**
