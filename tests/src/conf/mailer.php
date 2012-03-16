5b4dbed0dfb1e7672c5f3bece406d644bf001d26
Aleksey Antipov <aleskey.antipov@videinfra.com>
2012-03-16 13:59:36 +0000

    changes related to #5338 (unit-test configuration)
    git-svn-id: http://svn.vig/supra7/trunk@1872 f05b830f-2085-0410-a479-8f75d39d0b34

--- tests/src/conf/mailer.php
+++ tests/src/conf/mailer.php
@@ -3,5 +3,18 @@
 use Supra\ObjectRepository\ObjectRepository;
 
 $massMail = new \Supra\Mailer\MassMail\MassMail();
+
+//$username = 'sitesupra@videinfra.com';
+//$password = 'turnover';
+//
+//// Create new swift connection and authenticate
+//$transport = Swift_SmtpTransport::newInstance('smtp.sendgrid.net', 25);
+//$transport->setUsername($username);	
+//$transport->setPassword($password);
+//
+//$mailer = new \Supra\Mailer\Mailer($transport);
+//ObjectRepository::setDefaultMailer($mailer);
+//$massMail = new \Supra\Mailer\MassMail\MassMail();
+
 ObjectRepository::setCallerParent($massMail, 'Supra\Tests');
 ObjectRepository::setDefaultMassMail($massMail);
