13c94dc2068c62dddd029b2c0d5d28b4d4116387
Dmitry Polovka <dmitryp@videinfra.com>
2012-04-16 14:23:13 +0000

    Post workaround for files
    git-svn-id: http://svn.vig/supra7/trunk@2079 f05b830f-2085-0410-a479-8f75d39d0b34

--- src/lib/Supra/Controller/PreFilter/PostMaxSizeWorkaroundPreFilter.php
+++ src/lib/Supra/Controller/PreFilter/PostMaxSizeWorkaroundPreFilter.php
@@ -16,6 +16,7 @@ use Supra\ObjectRepository\ObjectRepository;
  */
 class PostMaxSizeWorkaroundPreFilter extends Controller\ControllerAbstraction implements Controller\PreFilterInterface
 {
+
 	/**
 	 * Main method
 	 */
@@ -27,48 +28,63 @@ class PostMaxSizeWorkaroundPreFilter extends Controller\ControllerAbstraction im
 			/* @var $request HttpRequest */
 			$post = $request->getPost();
 			if ($post->count() == 0) {
-			
+
 				$phpInput = fopen('php://input', 'rb');
-				
+
 				$inputContent = '';
 				while ( ! feof($phpInput)) {
 					$line = stream_get_line($phpInput, 1024, "\n");
-					
+
 					if (strpos($line, 'Content-Type:') !== false) {
+						$inputContent .= $line;
 						break;
 					}
-					
+
 					$inputContent .= $line;
 				}
-				
+
 				$postArray = $this->_parseRawPost($inputContent);
 				$request->setPost($postArray);
 			}
 		}
 	}
-	
+
 	// TODO: avoid usage of regular expression, if possible
-	private function _parseRawPost($rawPost) 
+	private function _parseRawPost($rawPost)
 	{
 		$request = $this->getRequest();
 		$contentType = $request->getServerValue('CONTENT_TYPE');
-		
+
 		preg_match('/boundary=(.*)$/', $contentType, $matches);
 		if ( ! isset($matches[1])) {
 			return array();
 		}
-		
+
 		$boundary = $matches[1];
 		$rawPostArray = explode($boundary, $rawPost);
-		
+
 		$post = array();
-		foreach($rawPostArray as $postItem) {
+		foreach ($rawPostArray as $postItem) {
 			$postItem = trim($postItem, '-');
 			if (preg_match('/ name="([^\".]+)\"[^;](.*)/', $postItem, $keyMatches) == 1 && count($keyMatches) > 2) {
 				$post[$keyMatches[1]] = trim($keyMatches[2]);
 			}
+
+			if (strpos($postItem, 'filename="') !== false) {
+				$matches = $output = array();
+				preg_match_all('/\S+=\"\S+\"/', $postItem, $matches);
+				if ( ! empty($matches[0])) {
+					$matches = $matches[0];
+					$query = join('&', $matches);
+					parse_str($query, $output);
+
+					if ( ! empty($output['name']) && ! empty($output['filename'])) {
+						$post[trim($output['name'], '"')] = trim($output['filename'], '"');
+					}
+				}
+			}
 		}
-		
+
 		return $post;
 	}
 
