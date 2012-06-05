15040ce2669040e0a23430a77787bcf5cd549f90
Aigars Gedroics <aigars.gedroics@videinfra.com>
2012-06-05 10:55:27 +0300

    List of trashed pages fixed

--- src/webroot/cms/content-manager/sitemaprecycle/SitemaprecycleAction.php
+++ src/webroot/cms/content-manager/sitemaprecycle/SitemaprecycleAction.php
@@ -38,9 +38,9 @@ class SitemaprecycleAction extends PageManagerAction
 	{
 		// Main
 		$this->restorePageVersion();
-		
+
 		// Audit log
-		$pageData =  $this->getPageLocalization();
+		$pageData = $this->getPageLocalization();
 		$this->writeAuditLog('%item% restored', $pageData);
 	}
 
@@ -53,102 +53,58 @@ class SitemaprecycleAction extends PageManagerAction
 
 		$auditEm = ObjectRepository::getEntityManager('#audit');
 
-		$trashRevisions = $auditEm->getRepository(PageRevisionData::CN())
-				->findByType(PageRevisionData::TYPE_TRASH);
+		$searchCriteria = array(
+			'locale' => $localeId,
+			'type' => PageRevisionData::TYPE_TRASH,
+		);
 
-		$trashRevisionsById = array();
-		if ( ! empty($trashRevisions)) {
-			// collecting ids
-			$revisionIds = array();
-			foreach ($trashRevisions as $revision) {
-				$revisionIds[] = $revision->getId();
-				$trashRevisionsById[$revision->getId()] = $revision;
+		$qb = $auditEm->createQueryBuilder()
+				->from($entity, 'l')
+				->from(PageRevisionData::CN(), 'r')
+				->select('l.id, l.title, l.revision, r.creationTime')
+				->andWhere('r.type = :type')
+				->andWhere('l.locale = :locale')
+				->andWhere('l.revision = r.id')
+				->orderBy('r.creationTime', 'DESC');
+
+		if ($entity == Entity\PageLocalization::CN()) {
+			$qb->addSelect('l.pathPart, l.template');
+		}
+
+		$localizationDataList = $qb->getQuery()
+				->execute($searchCriteria, \Doctrine\ORM\Query::HYDRATE_ARRAY);
+
+		foreach ($localizationDataList as $localizationData) {
+
+			$pageInfo = array();
+			$pathPart = null;
+			$templateId = null;
+
+			if ($entity == Entity\PageLocalization::CN()) {
+				$pathPart = $localizationData['pathPart'];
+				$templateId = $localizationData['template'];
 			}
 
-			$searchCriteria = array(
-				'locale' => $localeId,
-				'revision' => $revisionIds
+			$timeTrashed = $localizationData['creationTime']->format('Y-m-d');
+
+			$pageInfo = array(
+				'id' => $localizationData['id'],
+				'title' => $localizationData['title'],
+				'template' => $templateId,
+				'path' => $pathPart,
+				'revision' => $localizationData['revision'],
+				'date' => $timeTrashed,
+				// TODO: do we need this?
+				'icon' => 'page',
+				'localized' => true,
+				'published' => false,
+				'scheduled' => false,
 			);
-			
-			$dql = 'SELECT l.master FROM page:Abstraction\Localization l
-				WHERE l.locale = :locale
-				AND l.revision IN (:revision)';
-			
-			$masterIds = $auditEm->createQuery($dql)
-					->execute($searchCriteria, ColumnHydrator::HYDRATOR_ID);
-			
-			$masters = $auditEm->createQuery("SELECT m FROM page:Abstraction\AbstractPage m WHERE m.id IN (:id)")
-					->execute(array('id' => $masterIds));
 
-			foreach ($masters as $master) {
-
-				/* @var $master Entity\Abstraction\AbstractPage */
-				$pageLocalization = $master->getLocalization($localeId);
-				
-				$pageInfo = array();
-				$pathPart = null;
-				$templateId = null;
-
-				if ($pageLocalization instanceof Entity\PageLocalization) {
-					$pathPart = $pageLocalization->getPathPart();
-				}
-
-				if ($pageLocalization instanceof Entity\PageLocalization) {
-					$template = $pageLocalization->getTemplate();
-
-					if ($template instanceof Entity\Template) {
-						$templateId = $template->getId();
-					}
-				}
-
-				$pageRevisionId = $pageLocalization->getRevisionId();
-
-				$dateCreated = null;
-				if ($trashRevisionsById[$pageRevisionId] instanceof PageRevisionData) {
-					$revision = $trashRevisionsById[$pageRevisionId];
-					$dateCreated = $revision->getCreationTime()->format('Y-m-d');
-				}
-
-				$pageInfo = array(
-					'id'		=> $pageLocalization->getId(),
-					'title'		=> $pageLocalization->getTitle(),
-					'template'	=> $templateId,
-					'path'		=> $pathPart,
-					'revision'	=> $pageLocalization->getRevisionId(),
-					'localized' => true,
-					// TODO: hardcoded	
-					'published' => false,
-					'scheduled' => true,
-					'date'		=> $dateCreated,
-					'icon'		=> 'page',
-				);
-
-				$response[] = $pageInfo;
-			}
-
-			usort($response, array($this, 'sortByDeletionDateDesc'));
+			$response[] = $pageInfo;
 		}
 
 		return $response;
-	}
-
-	/**
-	 * Sorts page data by "revision creation date" (deletion date) 
-	 * 
-	 * @param array $a
-	 * @param array $b
-	 * @return array 
-	 */
-	public function sortByDeletionDateDesc($a, $b)
-	{
-		$a = $a['date'];
-		$b = $b['date'];
-		
-		if ($a == $b) {
-			return 0;
-		}
-		
-		return ($a > $b) ? -1 : 1;
 	}
 
 }
