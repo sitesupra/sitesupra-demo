200c2bed41aca79ad230694f9e460e85c08b2bc7
Dmitry Polovka <dmitry.polovka@videinfra.com>
2012-06-20 16:29:32 +0300

    Task #6481 Create block property editable type "visual drop-down"

--- src/webroot/components/Blocks/Gallery/GalleryBlock.php
+++ src/webroot/components/Blocks/Gallery/GalleryBlock.php
@@ -12,6 +12,11 @@ class GalleryBlock extends BlockController
 	public function doExecute()
 	{
 		$response = $this->getResponse();
+		$context = $response->getContext();
+		
+		$dir = str_replace(SUPRA_WEBROOT_PATH, '', __DIR__);
+		$context->addCssLinkToLayoutSnippet('css', "/{$dir}/css/style.css");
+		
 		$response->outputTemplate('index.html.twig');
 	}
 
