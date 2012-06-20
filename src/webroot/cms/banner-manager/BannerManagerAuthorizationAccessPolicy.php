491ab1674cf28f4b4d0fbb68badac12bd52f5b4b
Krists Krīgers <krists.krigers@videinfra.com>
2012-06-20 17:04:41 +0300

    Issue red #6689; * Fix authorization access policy for banner manager to remove ALL permissions to banner manager on "none";

--- src/webroot/cms/banner-manager/BannerManagerAuthorizationAccessPolicy.php
+++ src/webroot/cms/banner-manager/BannerManagerAuthorizationAccessPolicy.php
@@ -1,13 +1,25 @@
 <?php
 
-
 namespace Supra\Cms\BannerManager;
 
 use Supra\Authorization\AccessPolicy\AuthorizationAllOrNoneAccessPolicy;
 
-class BannerManagerAuthorizationAccessPolicy extends AuthorizationAllOrNoneAccessPolicy 
+class BannerManagerAuthorizationAccessPolicy extends AuthorizationAllOrNoneAccessPolicy
 {
 
-}	
+	protected function setApplicationAccessValue(AbstractUser $user, $applicationAccessValue)
+	{
+		if($applicationAccessValue == self::APPLICATION_ACCESS_NONE_VALUE) {
+			
+			$this->revokeApplicationAllAccessPermission($user);
+			$this->revokeApplicationSomeAccessPermission($user);
+			$this->revokeApplicationExecuteAccessPermission($user);
+		}
+		else {
+			
+			parent::setApplicationAccessValue($user, $applicationAccessValue);
+		}
+	}
 
+}
 
