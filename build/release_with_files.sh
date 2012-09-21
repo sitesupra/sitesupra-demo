40616c6c4d0b5c5f64c2f6aa1846a6e0d54a6876
Aigars Gedroics <aigars.gedroics@videinfra.com>
2012-09-21 17:32:37 +0300

    Release sh with file sync from UAT

---
+++ build/release_with_files.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+# Create release directory
+mkdir ${WORKSPACE}/release || echo "Folder already exists"
+cd ${WORKSPACE}/release
+
+# Remove old artifacts
+rm ${WORKSPACE}/*.zip || echo "No ZIP artifacts to remove"
+
+# Copy contents
+rsync -r --delete \
+  --copy-links \
+  --safe-links \
+  --exclude=.* \
+  --exclude=/src/files/* \
+  --exclude=/src/webroot/files/* \
+  --exclude=/src/log/* \
+  --exclude=/tests \
+  --exclude=/tmp \
+  --exclude=/src/webroot/dev \
+  --exclude /solr \
+  --exclude /release \
+  --exclude /supra7 \
+  ${WORKSPACE}/ .
+
+rsync -r --delete /var/www/vhosts/${JOB_NAME/%-release}-uat.vig/src/webroot/files/ ${WORKSPACE}/release/src/webroot/files/
+rsync -r --delete /var/www/vhosts/${JOB_NAME/%-release}-uat.vig/src/files/ ${WORKSPACE}/release/src/files/
+
+rm -rf ./tests
+rm -rf ./tmp
+rm -rf ./src/webroot/dev
+
+# Replace INI file
+cp ./src/conf/supra.release.ini ./src/conf/supra.ini.example
+rm ./src/conf/supra*.ini
+
+# Register version number
+echo ${project_version}.${BUILD_NUMBER} > ./VERSION
+
+zip -r ${WORKSPACE}/${JOB_NAME/%-release}.web.${project_version}.${BUILD_NUMBER}.zip *
