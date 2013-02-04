544ac53f3df289594ca96f85c01cc17f0203ca7c
Kaspars Zuks <kaspars.zuks@videinfra.com>
2013-02-04 18:25:37 +0200

    Advanced slideshow manager template

--- src/webroot/components/FancyBlocks/SlideshowAdvanced/data.js
+++ src/webroot/components/FancyBlocks/SlideshowAdvanced/data.js
@@ -26,82 +26,177 @@
 			"properties": [
 				// Properties for each slide
 				{
-					"id": "layout",
-					"type": "SelectVisual",
-					"separateSlide": true, // Show preview in the button, which opens another slide with all values
-					"values": [
-						{
-							// This is a group
-							"icon": "...",
-							"id": "group_background_text",
-							"title": "Background image and text",
-							"values": [
-								// These are values
-								...
-							]
-						},
-						{
-							// This is a group
-							"icon": "...",
-							"id": "group_background_text_graphics",
-							"title": "Background image, text and graphics element or a video",
-							"values": [
-								// These are values
+					'id': 'layout',
+	          		'type': 'SelectVisual',
+	      			'label': 'Layout',
+			  		'defaultValue': 'bg_text_left',
+			  		'separateSlide': true, // Show preview in the button, which opens another slide with all values
+			  		'values': [
+				  		{
+		  					'id': 'bg',
+		  					'title': 'Background image only',
+		  					'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg.png'
+		  				}, {
+		  					// This is a group
+							'id': 'bg_text',
+							'title': 'Background image and text',
+							'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text.png',
+							'values': [
+								// These are actual values
 								{
-									"icon": "...",
-									"id": "text_graphics",
-									"title": "..."
+									'id': 'bg_text_left',
+									'title': 'Text on left side',
+									'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text-left.png'
+								}, {
+									'id': 'bg_text_right',
+									'title': 'Text on right side',
+									'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text-right.png'
+								}, {
+									'id': 'bg_text_left_top',
+									'title': 'Text on left and top sides',
+									'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text-left-top.png'
+								}, {
+									'id': 'bg_text_right_top',
+									'title': 'Text on right and top sides',
+									'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text-right-top.png'
+								}, {
+									'id': 'bg_text_left_bottom',
+									'title': 'Text on left and bottom sides',
+									'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text-left-bottom.png'
+								}, {
+									'id': 'bg_text_right_top',
+									'title': 'Text on right and bottom sides',
+									'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text-right-bottom.png'
 								}
 							]
-						},
-						{
-							// This is a value, it can be on same level as groups
-							"icon": "...",
-							"id": "...",
-							"title": "Background image only"
-						},
-						...
+						}, {
+							'id': 'bg_text_img',
+							'title': 'Background image, text and graphics element',
+							'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text-img.png',
+							'values': [
+								{
+									'id': 'bg_text_img_left',
+									'title': 'Text on left side',
+									'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text-img-left.png'
+								}, {
+									'id': 'bg_text_img_right',
+									'title': 'Text on right side',
+									'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text-img-right.png'
+								}, {
+									'id': 'bg_text_img_left_top',
+									'title': 'Text on left and top sides',
+									'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text-img-left-top.png'
+								}, {
+									'id': 'bg_text_img_right_top',
+									'title': 'Text on right and top sides',
+									'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text-img-right-top.png'
+								}, {
+									'id': 'bg_text_img_left_bottom',
+									'title': 'Text on left and bottom sides',
+									'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text-img-left-bottom.png'
+								}, {
+									'id': 'bg_text_img_right_top',
+									'title': 'Text on right and bottom sides',
+									'icon': '/components/FancyBlocks/SlideshowAdvanced/icons/layout/bg-text-img-right-bottom.png'
+								}
+							]
+						}
 					]
-				},
-				{
-					"id": "background",
-					"type": "Image",
-					...
-				},
-				{
-					"id": "text_main",
-					"type": "InlineHTML",
-					...
-				},
-				{
-					"id": "text_top",
-					"type": "InlineHTML",
-					...
-				},
-				{
-					"id": "image",
-					"type": "InlineImage",
-					...
-				},
-				{
-					"id": "video",
-					"type": "Video",
-					...
-				}
+			  	}, {
+			  		'id': 'background',
+			  		'type': 'BlockBackground',
+			  		'label': 'Background image'
+			  	}, {
+			  		'id': 'text_main',
+			  		'type': 'InlineHTML',
+			  		'label': 'Main text',
+			  		'defaultValue': {
+			  			'data': {},
+			  			'html': '<h1>Lorem ipsum</h1><h2>Dolor sit amet</h2><p>Lid est laborum dolo es fugats untras. Et harums quidem rerum facilisdolores nemis omnis fugiats vitaro minimarerums unsers sadips dolores sitsers untra nemi amets.</p>'
+			  		},
+			  		'inline': true
+			  	}, {
+			  		'id': 'text_top',
+			  		'type': 'InlineHTML',
+			  		'label': 'Top text',
+			  		'defaultValue': {
+			  			'data': {},
+			  			'html': '<p>Lid est laborum dolo es fugats untras. Et harums quidem rerum facilisdolores nemis omnis fugiats vitaro minimarerums unsers sadips dolores sitsers untra nemi amets.</p>'
+			  		},
+			  		'inline': true
+			  	}, {
+			  		'id': 'media',
+			  		'type': 'InlineMedia', // New input type, but can be used only in slideshow manager!!!
+			  		'label': 'Image or video'
+			  	}, {
+			  		'id': 'buttons',
+			  		'type': 'Set', // New input type, can be used in all CMS!!!
+			  		'label': 'Buttons',
+			  		'labelAdd': 'Add more buttons',
+			  		'labelRemove': 'Remove button',
+			  		'labelItem': 'Button %s',
+			  		'properties': [
+			  			{
+			  				'id': 'title',
+			  				'type': 'String',
+			  				'label': 'Title'
+			  			},
+			  			{
+			  				'id': 'link',
+			  				'type': 'Link',
+			  				'label': 'Link'
+			  			}
+			  		]
+			  	}
 			],
 			"layouts": [
 				// All layouts, other blocks does not have this property, something new!
+				// HTML should be taken from template files
 				{
-					"id": "text_graphics",
-					"html": '<div class="as-wrapper">\
-								<div class="text">\
-									...something...\
-								</div>\
-								<div class="video">\
-									...something...\
-								</div>\
-							</div>'
-				}
+					'id': 'bg',
+          			'html': '<li><div class="as-wrapper"><img class="as-layer absolute fill" src="{{ property.background }}" /></div></li>'
+          		},
+          		
+          		{
+					'id': 'bg_text_left',
+          			'html': '<li><div class="as-wrapper"><img class="as-layer absolute fill" src="{{ property.background }}" /><div class="as-layer as-layer-left-small">{{ property.text_main }}<div class="as-layer buttons" data-supra-item-property="buttons"></div></div><div class="as-layer as-layer-right-large">&nbsp;</div></div></li>'
+          		}, {
+					'id': 'bg_text_right',
+          			'html': '<li><div class="as-wrapper"><img class="as-layer absolute fill" src="{{ property.background }}" /><div class="as-layer as-layer-left-large">&nbsp;</div><div class="as-layer as-layer-right-small">{{ property.text_main }}<div class="as-layer buttons" data-supra-item-property="buttons"></div></div></li>'
+          		}, {
+          			'id': 'bg_text_left_top',
+          			'html': '<li><div class="as-wrapper"><img class="as-layer absolute fill" src="{{ property.background }}" /><div class="as-layer as-layer-top">{{ property.text_top }}</div><div class="as-layer as-layer-left-small">{{ property.text_main }}<div class="as-layer buttons" data-supra-item-property="buttons"></div></div><div class="as-layer as-layer-right-large">&nbsp;</div></div></li>'
+          		}, {
+					'id': 'bg_text_right_top',
+          			'html': '<li><div class="as-wrapper"><img class="as-layer absolute fill" src="{{ property.background }}" /><div class="as-layer as-layer-top">{{ property.text_top }}</div><div class="as-layer as-layer-left-large">&nbsp;</div><div class="as-layer as-layer-right-small">{{ property.text_main }}<div class="as-layer buttons" data-supra-item-property="buttons"></div></div></li>'
+          		}, {
+          			'id': 'bg_text_left_bottom',
+          			'html': '<li><div class="as-wrapper"><img class="as-layer absolute fill" src="{{ property.background }}" /><div class="as-layer as-layer-left-small">{{ property.text_main }}<div class="as-layer buttons" data-supra-item-property="buttons"></div></div><div class="as-layer as-layer-right-large">&nbsp;</div><div class="as-layer as-layer-bottom">{{ property.text_top }}</div></li>'
+          		}, {
+					'id': 'bg_text_right_bottom',
+          			'html': '<li><div class="as-wrapper"><img class="as-layer absolute fill" src="{{ property.background }}" /><div class="as-layer as-layer-left-large">&nbsp;</div><div class="as-layer as-layer-right-small">{{ property.text_main }}<div class="as-layer buttons" data-supra-item-property="buttons"></div></div><div class="as-layer as-layer-bottom">{{ property.text_top }}</div></li>'
+          		},
+          		
+          		
+          		{
+					'id': 'bg_text_img_left',
+          			'html': '<li><div class="as-wrapper"><img class="as-layer absolute fill" src="{{ property.background }}" /><div class="as-layer as-layer-left-small">{{ property.text_main }}<div class="as-layer buttons" data-supra-item-property="buttons"></div></div><div class="as-layer as-layer-right-large">{{ property.media }}</div></div></li>'
+          		}, {
+					'id': 'bg_text_img_right',
+          			'html': '<li><div class="as-wrapper"><img class="as-layer absolute fill" src="{{ property.background }}" /><div class="as-layer as-layer-left-large">{{ property.media }}</div><div class="as-layer as-layer-right-small">{{ property.text_main }}<div class="as-layer buttons" data-supra-item-property="buttons"></div></div></li>'
+          		}, {
+          			'id': 'bg_text_img_left_top',
+          			'html': '<li><div class="as-wrapper"><img class="as-layer absolute fill" src="{{ property.background }}" /><div class="as-layer as-layer-top">{{ property.text_top }}</div><div class="as-layer as-layer-left-small">{{ property.text_main }}<div class="as-layer buttons" data-supra-item-property="buttons"></div></div><div class="as-layer as-layer-right-large">{{ property.media }}</div></li>'
+          		}, {
+					'id': 'bg_text_img_right_top',
+          			'html': '<li><div class="as-wrapper"><img class="as-layer absolute fill" src="{{ property.background }}" /><div class="as-layer as-layer-top">{{ property.text_top }}</div><div class="as-layer as-layer-left-large">{{ property.media }}</div><div class="as-layer as-layer-right-small">{{ property.text_main }}<div class="as-layer buttons" data-supra-item-property="buttons"></div></div></li>'
+          		}, {
+          			'id': 'bg_text_img_left_bottom',
+          			'html': '<li><div class="as-wrapper"><img class="as-layer absolute fill" src="{{ property.background }}" /><div class="as-layer as-layer-left-small">{{ property.text_main }}<div class="as-layer buttons" data-supra-item-property="buttons"></div></div><div class="as-layer as-layer-right-large">{{ property.media }}</div><div class="as-layer as-layer-bottom">{{ property.text_top }}</div></li>'
+          		}, {
+					'id': 'bg_text_img_right_bottom',
+          			'html': '<li><div class="as-wrapper"><img class="as-layer absolute fill" src="{{ property.background }}" /><div class="as-layer as-layer-left-large">{{ property.media }}</div><div class="as-layer as-layer-right-small">{{ property.text_main }}<div class="as-layer buttons" data-supra-item-property="buttons"></div></div><div class="as-layer as-layer-bottom">{{ property.text_top }}</div></li>'
+          		}
 			]
 			...
 		}
@@ -130,10 +225,27 @@
 	]
 }
 
-// Slide ifno
+// Slide info
 {
-	"layout": "text_graphics",
+	"layout": "bg_text_img_left",
 	"background": ...,
-	"text_main": ...,
-	"text-_top": ...
+	"text_main": {
+		"data": {...},
+		"html": "..."
+	},
+	"text_top": {
+		"data": {...},
+		"html": "..."
+	},
+	"media": {
+		"type": "video",
+		"resource": "source",
+		"source": "<embed ....>"
+	},
+	"button": [
+		{
+			"title": "My button",
+			"link": ""
+		}
+	]
 }
