2de6f0f373020bebd32c22ca0149b899adbd09c4
Eduard <eduard@videinfra.com>
2013-02-16 12:15:09 +0200

    Fixing some template errors

--- src/webroot/components/FancyBlocks/Slideshow/SlideshowBlock.php
+++ src/webroot/components/FancyBlocks/Slideshow/SlideshowBlock.php
@@ -6,23 +6,8 @@ use Supra\Controller\Pages\BlockController;
 
 class SlideshowBlock extends BlockController
 {
-
-	/**
-	 * @return array
-	 */
-	public static function getPropertyDefinition()
-	{
-		$properties = array();
-
-		return $properties;
-	}
-
 	protected function doExecute()
 	{
-		$response = $this->getResponse();
-		/* @var $response \Supra\Response\TwigResponse */
-
-		$response->outputTemplate('index.html.twig');
+		$this->getResponse()->outputTemplate('index.html.twig');
 	}
-
 }
