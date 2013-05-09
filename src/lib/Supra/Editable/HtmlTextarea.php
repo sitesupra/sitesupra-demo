eb692372a2ddf14bfe62c3e5cc0679e647a367f0
Eduard <eduard@videinfra.com>
2013-05-09 15:48:46 +0300

    New type - html textarea

---
+++ src/lib/Supra/Editable/HtmlTextarea.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Supra\Editable;
+
+/**
+ * Textarea with wysiwyg editor
+ */
+class HtmlTextarea extends Textarea
+{
+	const EDITOR_TYPE = 'Html';
+	const EDITOR_INLINE_EDITABLE = false;
+}
