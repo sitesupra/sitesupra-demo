cd4c2038a3662ae76dfa0025b09d9bbd1bed8eca
Kaspars Zuks <kaspars.zuks@videinfra.com>
2013-06-03 10:48:38 +0300

    #10781 Blog post list

---
+++ src/webroot/components/FancyBlocks/BlogPostList/BlogPostListBlock.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Project\FancyBlocks\BlogPostList;
+
+use Supra\Controller\Pages\BlockController;
+use Supra\Controller\Pages\Entity\ApplicationPage;
+use Supra\Controller\Pages\Blog\BlogApplication;
+
+class BlogPostListBlock extends BlockController
+{
+	/**
+	 * @var \Supra\Controller\Pages\Blog\BlogApplication
+	 */
+	protected $blogApplication;
+	
+	
+	protected function doExecute()
+	{
+		$response = $this->getResponse();
+		/* @var $response \Supra\Response\TwigResponse */
+		
+		$response->assign('totalPages', 20);
+		$response->assign('currentPage', 19); // 0 index
+		$response->outputTemplate('index.html.twig');
+	}
+}
