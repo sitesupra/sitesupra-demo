256e57565f611435a10e750fd00d25037be9c6b4
Andrei Yakovlev <andrei@videinfra.com>
2013-06-14 13:26:40 +0300

    Task #10943: Moved 500 error function to bootstrap.php

--- src/webroot/index.php
+++ src/webroot/index.php
@@ -1,13 +1,5 @@
 <?php
 
-function outputInternalServerError()
-{
-    header('Content-Type: text/html; charset=utf-8');
-    $errorPage = file_get_contents('500.html');
-    echo $errorPage;
-    die();
-}
-
 // Bootstrap supra
 define('SUPRA_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 require_once SUPRA_PATH . 'lib/Supra/bootstrap.php';
