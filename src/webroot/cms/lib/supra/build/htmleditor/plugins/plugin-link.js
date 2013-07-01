52421d2e5b1becd629b9e65f0cdf11fbe950c565
Kaspars Zuks <kaspars.zuks@videinfra.com>
2013-07-01 19:14:14 +0300

    #11164 Link button style switch fix

--- src/webroot/cms/lib/supra/build/htmleditor/plugins/plugin-link.js
+++ src/webroot/cms/lib/supra/build/htmleditor/plugins/plugin-link.js
@@ -148,7 +148,9 @@ YUI().add('supra.htmleditor-plugin-link', function (Y) {
 		editLinkConfirmed: function (data, target) {
 			if (data && data.href) {
 				data.type = this.NAME;
-				this.htmleditor.setData(target, data);
+				
+				//Silently update data, we will trigger change manually
+				this.htmleditor.setData(target, data, true);
 				
 				//Title attribute
 				target.setAttribute('title', data.title || '');
