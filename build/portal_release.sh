58f6e330277f52d043dde09d40ecdef1d4df9a73
eduard <eduard@eduard>
2013-07-23 13:48:19 +0300

    Task #11449: Modify release build job in accordance with current architecture

--- build/portal_release.sh
+++ build/portal_release.sh
@@ -2,29 +2,53 @@
 
 set -e
 
-# Compile less files outside the CMS folder
-cd ${WORKSPACE}/release/src/webroot
+# Create release directory
+mkdir ${WORKSPACE}/release || echo "Folder already exists"
+cd ${WORKSPACE}/release
+
+# Remove old artifacts
+rm ${WORKSPACE}/*.zip || echo "No ZIP artifacts to remove"
+
+# Copy contents
+rsync -r --delete \
+  --copy-links \
+  --safe-links \
+  --exclude=.* \
+  --exclude=/src/files/* \
+  --exclude=/src/webroot/files/* \
+  --exclude=/src/log/* \
+  --exclude=/tests \
+  --exclude=/tmp \
+  --exclude=/src/webroot/dev \
+  --exclude /solr \
+  --exclude /release \
+  --exclude /supra7 \
+  ${WORKSPACE}/ .
+
+rm -rf ./tests
+rm -rf ./tmp
+rm -rf ./src/webroot/dev
+
+# Replace INI file
+cp ./src/conf/supra.release.ini ./src/conf/supra.ini.example
+rm ./src/conf/supra*.ini
+
+# Register version number
+echo ${project_version}.${BUILD_NUMBER} > ./VERSION
+
+cd src/webroot/
+find ./cms/ -name "*.css.less" -exec php cms/lib/supra/combo/combo_pregenerate.php {} \; 
+find ./cms/ -name "*.css.less" -delete
+
+if [ -d "cms-local" ]
+then
+	find ./cms-local/ -name "*.css.less" -exec php cms/lib/supra/combo/combo_pregenerate.php {} \; 
+	find ./cms-local/ -name "*.css.less" -delete
+fi
 
 find ./ -name "*.css.less" -exec php ./cms/lib/supra/combo/combo_pregenerate.php {} \;
 find ./ -name "*.css.less" -delete
 
-# Create temporary directory for webroot static files
-#mkdir ${WORKSPACE}/release-static || echo "Folder already exists"
-#cd ${WORKSPACE}/release-static
+cd ${WORKSPACE}/release
 
-# Copy static files from project's webroot to release-static folder
-#rsync -r --delete \
-#  --copy-links \
-#  --safe-links \
-#  --exclude=.* \
-#  --exclude=/components \
-#  --exclude=/components-supra7 \
-#  --exclude=*.php \
-#  --exclude=/dev \
-#  ${WORKSPACE}/release/src/webroot/ .
-
-# Zip static files
-#zip -r ${WORKSPACE}/${JOB_NAME%-release}.web-static.${project_version}.${BUILD_NUMBER}.zip *
-
-# Remove temporary folder
-#rm -rf ${WORKSPACE}/release-static
+zip -r ${WORKSPACE}/${JOB_NAME/%-release}.web.${project_version}.${BUILD_NUMBER}.zip *
