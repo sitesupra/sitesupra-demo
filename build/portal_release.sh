748ed4f0d6102b52e383ecb320bb4684226dd52c
eduard <eduard@eduard>
2013-07-23 13:42:12 +0300

    Task #11449: Modify release build job in accordance with current architecture  - "web-static" create process commented out, as it is not required anymore

--- build/portal_release.sh
+++ build/portal_release.sh
@@ -9,22 +9,22 @@ find ./ -name "*.css.less" -exec php ./cms/lib/supra/combo/combo_pregenerate.php
 find ./ -name "*.css.less" -delete
 
 # Create temporary directory for webroot static files
-mkdir ${WORKSPACE}/release-static || echo "Folder already exists"
-cd ${WORKSPACE}/release-static
+#mkdir ${WORKSPACE}/release-static || echo "Folder already exists"
+#cd ${WORKSPACE}/release-static
 
 # Copy static files from project's webroot to release-static folder
-rsync -r --delete \
-  --copy-links \
-  --safe-links \
-  --exclude=.* \
-  --exclude=/components \
-  --exclude=/components-supra7 \
-  --exclude=*.php \
-  --exclude=/dev \
-  ${WORKSPACE}/release/src/webroot/ .
+#rsync -r --delete \
+#  --copy-links \
+#  --safe-links \
+#  --exclude=.* \
+#  --exclude=/components \
+#  --exclude=/components-supra7 \
+#  --exclude=*.php \
+#  --exclude=/dev \
+#  ${WORKSPACE}/release/src/webroot/ .
 
 # Zip static files
-zip -r ${WORKSPACE}/${JOB_NAME%-release}.web-static.${project_version}.${BUILD_NUMBER}.zip *
+#zip -r ${WORKSPACE}/${JOB_NAME%-release}.web-static.${project_version}.${BUILD_NUMBER}.zip *
 
 # Remove temporary folder
-rm -rf ${WORKSPACE}/release-static
+#rm -rf ${WORKSPACE}/release-static
