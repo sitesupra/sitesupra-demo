35864214637db86584633791904e03db82faa068
Eduard <eduard@videinfra.com>
2014-04-22 20:12:57 +0300

    Revert "EncoderEventListener now listens to BlockEvents 'blockEndExecuteEvent' instead of PageContoller 'postPrepareContent'"
    This reverts commit ce1cddba3e209005185dd2b0d89d5e1a35ff313a.

--- src/lib/Supra/Controller/Pages/Email/EmailEncoder.php
+++ src/lib/Supra/Controller/Pages/Email/EmailEncoder.php
@@ -2,7 +2,7 @@
 
 namespace Supra\Controller\Pages\Email;
 
-use Supra\Controller\Pages\Event\BlockEvents;
+use Supra\Controller\Pages\PageController;
 
 /**
  */
@@ -12,37 +12,37 @@ class EmailEncoder
 	 * @var self
 	 */
 	private static $instance;
-
+	
 	/**
 	 */
 	public static function getInstance()
 	{
 		if (self::$instance === null) {
-
+			
 			self::$instance = new self();
 
 			$em = \Supra\ObjectRepository\ObjectRepository::getEventManager();
-
-			$alreadySubscribed = $em->getListeners(BlockEvents::blockEndExecuteEvent);
-
+			
+			$alreadySubscribed = $em->getListeners(PageController::EVENT_POST_PREPARE_CONTENT);
+			
 			foreach ($alreadySubscribed as $listener) {
 				if ($listener[0] instanceof EncoderEventListener) {
-					$em->removeListener(BlockEvents::blockEndExecuteEvent, $listener[0]);
+					$em->removeListener(PageController::EVENT_POST_PREPARE_CONTENT, $listener[0]);
 				}
 			}
-
-			$em->listen(BlockEvents::blockEndExecuteEvent, new EncoderEventListener());
+			
+			$em->listen(PageController::EVENT_POST_PREPARE_CONTENT, new EncoderEventListener());
 		}
-
+		
 		return self::$instance;
 	}
-
+	
 	/**
 	 * @param string $email
 	 * @return string
 	 */
 	public function encode($email)
-	{
+	{		
 		return str_rot13($email);
 	}
 }
--- src/lib/Supra/Controller/Pages/Email/EncoderEventListener.php
+++ src/lib/Supra/Controller/Pages/Email/EncoderEventListener.php
@@ -2,32 +2,25 @@
 
 namespace Supra\Controller\Pages\Email;
 
-use Supra\Controller\Pages\Event\BlockEventsArgs;
-use Supra\Controller\Pages\Listener\BlockExecuteListener;
+use Supra\Controller\Pages\Event\PostPrepareContentEventArgs;
 
 /**
  */
 class EncoderEventListener
-{
-
+{	
 	const DECODER_JAVASCRIPT_FILE_URI = '/cms/lib/public/decipher-email.min.js';
-
+	
 	/**
 	 * @var boolean
 	 */
 	private $flushed = false;
-
-	/**
-	 * @param BlockEventsArgs $eventArgs
-	 */
-	public function blockEndExecuteEvent(BlockEventsArgs $eventArgs)
+	
+	public function postPrepareContent(PostPrepareContentEventArgs $eventArgs)
 	{
-		if ( ! $this->flushed
-				&& $eventArgs->actionType === BlockExecuteListener::ACTION_CONTROLLER_EXECUTE) {
-
-			$context = $eventArgs->blockController->getResponse()
-					->getContext();
-
+		if ( ! $this->flushed) {
+			
+			$context = $eventArgs->response->getContext();
+			
 			$context->addJsUrlToLayoutSnippet('js', self::DECODER_JAVASCRIPT_FILE_URI);
 			$this->flushed = true;
 		}
