7c60e7e955141f8454074f9d0bf3ecd24b971c4e
Aleksandrs Eigims <aleksandr.e@aleksandre.(none)>
2014-06-12 12:10:45 +0300

    CLI configuration update - test environment is now being initiated only with command arguments

--- bin/cli-config.php
+++ bin/cli-config.php
@@ -18,39 +18,36 @@ if (realpath($rootDir . '/bin/' . $baseName) !== realpath(__FILE__)) {
 
 define('SUPRA_PATH', $rootDir . DIRECTORY_SEPARATOR . 'src/');
 
-//require_once __DIR__ . '/../src/lib/Supra/bootstrap.php';
-
-// This loads test connection as well.
-// We need test connection because of fixtures now.
-$testBootstrap = $rootDir . '/tests/bootstrap.php';
-
-if (file_exists($testBootstrap) && file_exists($rootDir . '/tests/src/conf/supra.ini')) {
-	require_once $testBootstrap;
-} else {
-	require_once $rootDir . '/src/lib/Supra/bootstrap.php';
-}
-
+$bootstrap = $rootDir . '/src/lib/Supra/bootstrap.php';
 $namespace = '';
 
 if ( ! empty($_SERVER['argv'][1])) {
-	
+
 	$match = true;
-	
+
 	switch ($_SERVER['argv'][1]) {
 		case 'public':
 			$namespace = PageController::SCHEMA_PUBLIC;
 			break;
-		
+
 		case 'draft':
 		case 'cms':
 			$namespace = PageController::SCHEMA_DRAFT;
 			break;
-		
+
 		case 'test':
 		case 'tests':
+
 			$namespace = '#tests';
+
+			$testBootstrap = $rootDir . '/tests/bootstrap.php';
+
+			if (file_exists($testBootstrap) && file_exists($rootDir . '/tests/src/conf/supra.ini')) {
+				$bootstrap = $testBootstrap;
+			}
+
 			break;
-		
+
 		case 'audit':
 			$namespace = PageController::SCHEMA_AUDIT;
 			break;
@@ -58,21 +55,23 @@ if ( ! empty($_SERVER['argv'][1])) {
 		case 'users':
 			$namespace = 'Supra\User';
 			break;
-		
+
 		default:
 			$match = false;
 	}
-	
+
 	if ($match) {
 		array_splice($_SERVER['argv'], 1, 1);
 	}
 }
 
-$em = ObjectRepository::getEntityManager($namespace);
+require_once $bootstrap;
+
+$entityManager = ObjectRepository::getEntityManager($namespace);
 
 $helpers = array(
-	'db' => new ConnectionHelper($em->getConnection()),
-	'em' => new EntityManagerHelper($em),
+	'db' => new ConnectionHelper($entityManager->getConnection()),
+	'em' => new EntityManagerHelper($entityManager),
 	'dialog' => new DialogHelper(),
 );
 $helperSet = new HelperSet($helpers);
