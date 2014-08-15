faff9edd9e4120c7576aee85440a7db7013a9b21
eduard <eduard@eduard>
2014-08-15 18:35:15 +0300

    - Blog Comments fancy block version up (taken from supra7 layout repo)

--- src/webroot/components/FancyBlocks/BlogComments/BlogCommentsBlock.php
+++ src/webroot/components/FancyBlocks/BlogComments/BlogCommentsBlock.php
@@ -14,130 +14,138 @@ class BlogCommentsBlock extends BlockController
 	{
 		$response = $this->getResponse();
 		/* @var $response \Supra\Response\TwigResponse */
-		
+
 		$blogApplication = $this->getBlogApplication();
 		if ( ! $blogApplication instanceof BlogApplication) {
 			$response->outputTemplate('application-missing.html.twig');
 			return null;
 		}
-		
+
 		$request = $this->getRequest();
 		/* @var $request \Supra\Controller\Pages\Request\PageRequest */
-		if ($request->isPost() 
+		if ($request->isPost()
 				&& $request->getQuery()->offsetExists('postComment')) {
-			
+
 			$this->handleCommentPostRequest();
 		}
-		
-//		// @FIXME: remove comment action must request for blog app manager, not blocks
-//		// current implementation does not checks user permissions
-//		if ($request->isBlockRequest()) {
-//			
-//			$query = $request->getQuery();
-//			
-//			if ($query->has('comment_id') 
-//					&& $query->get('action', null) == 'remove_comment') {
-//			
-//				$commentId = $query->get('comment_id');
-//				$blogApplication->removeCommentById($commentId);
-//			}
-//		}
-        	
-		$postComments = array();
-		
+
+      	$postComments = array();
+
 		$localization = $request->getPageLocalization();
-		
+
 		if ($localization instanceof Entity\PageLocalization) {
 			$postComments = $blogApplication->getCommentsForLocalization($localization, $request instanceof PageRequestEdit);
 		}
-			
+
 		$applicationLocalizationId = $blogApplication->getApplicationLocalization()->getId();
-        
+
         $response->assign('comments', $postComments)
                 ->assign('blogApplicationLocalizationId', $applicationLocalizationId)
                 ->outputTemplate('index.html.twig');
 	}
-	
+
 	/**
 	 * @return \Supra\Controller\Pages\Blog\BlogApplication
 	 */
 	private function getBlogApplication()
 	{
 		$request = $this->getRequest();
-		
+
 		$parentPage = $request->getPageLocalization()
 				->getMaster()
 				->getParent();
-		
+
 		if ($parentPage instanceof Entity\ApplicationPage) {
-			
+
 			$em = \Supra\ObjectRepository\ObjectRepository::getEntityManager($this);
 			$localization = $parentPage->getLocalization($request->getLocale());
-			
+
 			$application = \Supra\Controller\Pages\Application\PageApplicationCollection::getInstance()
 					->createApplication($localization, $em);
-			
+
 			return $application;
 		}
 
 		return null;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	protected function handleCommentPostRequest()
 	{
+		$info = \Supra\ObjectRepository\ObjectRepository::getSystemInfo($this);
+		/* @var $info \SupraSite\SiteInfo\Info */
+
+		if ($info instanceof \SupraSite\SiteInfo\Info && $info->isDemo()) {
+			return;
+		}
+
 		$request = $this->getRequest();
 		/* @var $request \Supra\Request\HttpRequest */
 		$post = $request->getPost();
-		
+
 		$errors = array();
-		
-		$name = $post->get('name', null);
+
+		$name = trim($post->get('name', ''));
 		$email = $post->getValidOrNull('email', \Supra\Validator\Type\AbstractType::EMAIL);
-		$comment = $post->get('comment', null);
-		
+		$comment = trim($post->get('comment', ''));
+		$website = trim($post->get('url', ''));
+
 		if (empty($name)) {
-			$errors[] = 'Name is required';
+			$errors['name'] = 'Name is required';
 		}
-		
+
 		if (empty($email)) {
-			$errors[] = 'Valid email is required';
+			$errors['email'] = 'Valid email is required';
 		}
-		
+
 		if (empty($comment)) {
-			$errors[] = 'Comment is required';
+			$errors['comment'] = 'Comment is required';
 		}
-		
+
 		if ( ! empty($errors)) {
 			$this->getResponse()
-					->assign('errors', $errors);
-			
+					->assign('errors', $errors)
+					->assign('values', array(
+						'name' => $name,
+						'url' => $website,
+						'email' => $email,
+						'comment' => $comment,
+					));
+
 			return;
 		}
-		
-		$website = $post->get('url', null);
-		
+
 		$blogApp = $this->getBlogApplication();
-		
+
 		$blogComment = $blogApp->createComment();
-		
+
 		$blogComment->setAuthorName($name);
 		$blogComment->setAuthorEmail($email);
 		$blogComment->setAuthorWebsite($website);
-		
+
 		// @TODO: more advanced validation?
 		$comment = mb_substr($comment, 0, 1000);
-		
+
 		$blogComment->setComment($comment);
-		
+
 		$localization = $this->getRequest()
 				->getPageLocalization();
-		
+		/* @var $localization Entity\PageLocalization */
+
 		$blogComment->setPageLocalization($localization);
-		
+
 		$blogApp->storeComment($blogComment);
+
+		// Redirect to self
+		// @TODO: should we show 'comment successfully added' message?
+		$selfPath = $localization->getFullPath(\Supra\Uri\Path::FORMAT_LEFT_DELIMITER);
+
+		$this->getResponse()
+				->redirect($selfPath);
+
+		throw new \Supra\Controller\Exception\StopRequestException;
 	}
-	
+
 }
