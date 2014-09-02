7945b7d7c925d75fe5880cb9f9983ed79b096de4
Andrew Tch <andrew.tchircoff@gmail.com>
2014-09-02 18:43:19 +0300

    containeraware test

--- src/lib/Supra/Core/Console/Application.php
+++ src/lib/Supra/Core/Console/Application.php
@@ -2,9 +2,17 @@
 
 namespace Supra\Core\Console;
 
+use Supra\Core\DependencyInjection\Container;
+use Supra\Core\DependencyInjection\ContainerAware;
 use Symfony\Component\Console\Application as BaseApplication;
 
-class Application extends BaseApplication
+class Application extends BaseApplication implements ContainerAware
 {
     
+    protected $container;
+    
+    public function setContainer(Container $container)
+    {
+        $this->container = $container;
+    }
 }
