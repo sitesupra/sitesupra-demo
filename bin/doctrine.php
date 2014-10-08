87f32645fb992e4f535bfc16ae452c496df13400
Andrew Tch <andrew.tchircoff@gmail.com>
2014-10-08 17:56:27 +0300

    removed obsolete bin/; build/; directories

--- bin/doctrine.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$helperSet = null;
-
-require_once __DIR__ . '/cli-config.php';
-
-\Doctrine\ORM\Tools\Console\ConsoleRunner::run($helperSet);
--- build/dump.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd ${WORKSPACE}
-mysqldump -h db.vig -u dev --password=dev --default-character-set=utf8 ${JOB_NAME/%-release}_uat > ${JOB_NAME/%-release}.dump.${project_version}.${BUILD_NUMBER}.sql
-zip ${JOB_NAME/%-release}.dump.${project_version}.${BUILD_NUMBER}.zip ${JOB_NAME/%-release}.dump.${project_version}.${BUILD_NUMBER}.sql
-rm ${JOB_NAME/%-release}.dump.${project_version}.${BUILD_NUMBER}.sql
