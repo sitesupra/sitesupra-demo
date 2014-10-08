6653b72b63eb581b7ea7bd0e58945af7ab156d1b
eduard <eduard@eduard>
2014-10-08 12:20:20 +0300

    - minor Combo update to support both, JS and CSS (or LESS) assets

--- src/lib/Supra/Package/Framework/Controller/ComboController.php
+++ src/lib/Supra/Package/Framework/Controller/ComboController.php
@@ -7,30 +7,64 @@ use Assetic\Asset\FileAsset;
 use Supra\Core\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Assetic\Filter\LessFilter;
+use Assetic\Filter\CssRewriteFilter;
 
 class ComboController extends Controller
 {
 	public function comboAction(Request $request)
 	{
-		$collection = new AssetCollection();
+		$paths = explode('&', $request->attributes->get('paths'));
 
-		$paths = $request->attributes->get('paths');
-
-		$paths = explode('&', $paths);
+		$javaScriptAssets = $cssAssets
+				= array();
+		
+		$webRoot = $this->container->getApplication()->getWebRoot();
 
 		//@todo: support multiple assets instead of hack above
 		//@todo: compress / filter assets
 		foreach ($paths as $asset) {
-			$asset = $this->container->getApplication()->getWebRoot().DIRECTORY_SEPARATOR.$asset;
-			$collection->add(new FileAsset($asset, array(), $this->container->getApplication()->getWebRoot()));
+
+			$assetPath = $webRoot . DIRECTORY_SEPARATOR .  $asset;
+
+			if (($extension = pathinfo($assetPath, PATHINFO_EXTENSION)) === 'css'
+					|| $extension === 'less') {
+
+				if ($extension === 'css'
+						&& ! file_exists($assetPath)
+						&& file_exists($assetPath . '.less')) {
+
+					$assetPath = $assetPath . '.less';
+				}
+
+				$cssAssets[] = new FileAsset($assetPath, array(), $webRoot);
+				
+				continue;
+			}
+
+			$javaScriptAssets[] = new FileAsset($assetPath, array(), $webRoot);
 		}
+
+		if (empty($javaScriptAssets) && empty($cssAssets)) {
+			throw new ResourceNotFoundException();
+
+		} elseif (! empty($javaScriptAssets) && ! empty($cssAssets)) {
+			
+			throw new \LogicException('You cannot request combination of different asset types.');
+		}
+
+		$collection = ! empty($javaScriptAssets)
+				? new AssetCollection($javaScriptAssets)
+				: new AssetCollection($cssAssets, array(new LessphpFilter, new CssRewriteFilter));
 
 		$content = $this->container->getCache()->fetch('combo', $paths, function () use ($collection) {
 			return $collection->dump();
 		}, $collection->getLastModified());
 
-		return new Response($content, 200, array('Content-Type' => 'text/javascript'));
+		$contentType = ! empty($javaScriptAssets) ? 'text/javascript' : 'text/css';
 
+		return new Response($content, 200, array('Content-Type' => $contentType));
 	}
 
 }
