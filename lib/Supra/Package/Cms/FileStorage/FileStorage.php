1d9a9739d1dfd35969d613c01e313366a8aa74da
Andrew Tch <andrew.tchircoff@gmail.com>
2014-11-05 14:48:33 +0200

    removing of files

--- lib/Supra/Package/Cms/FileStorage/FileStorage.php
+++ lib/Supra/Package/Cms/FileStorage/FileStorage.php
@@ -1495,9 +1495,9 @@ class FileStorage implements ContainerAware
 
 	/**
 	 * Remove file from database and file system
-	 * @param Entity\File $file 
+	 * @param File $file
 	 */
-	private function removeFile(Entity\File $file)
+	private function removeFile(File $file)
 	{
 		$this->removeFileInFileSystem($file);
 
